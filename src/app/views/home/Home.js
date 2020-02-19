643e26b1d5606fc22562d7acf841a6709f7da3a5
Alyssa Darjuan <adarjuan97@gmail.com>
2020-02-19 02:08:01 -0800

    first animation test

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import { useSpring, animated } from 'react-spring';
 import './Home.scss';
 
 import { Button } from 'react-bootstrap';
@@ -8,7 +10,9 @@ import FAQs from '../../components/faqs/FAQs';
 import Team from '../../components/meet-team/team';
 
 import venushacks_logo from 'assets/images/logo_solid.png';
-import astronaut from 'assets/images/astronaut_with_laptop.png';
+import astronaut_laptop from 'assets/images/astronaut_with_laptop.png';
+import astronaut from 'assets/images/astronaut_solid.png';
+import laptop from 'assets/images/laptop.png';
 
 import periwinkle_planet from 'assets/images/periwinkleplanet.png';
 import about_title from 'assets/images/about.png';
@@ -23,15 +27,37 @@ import informatics from 'assets/images/sponsors/informatics.png';
 import google from 'assets/images/sponsors/google.png';
 import oracle from 'assets/images/sponsors/oracle.png';
 
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x, y) => `translate3d(${x / 20}px,${y / 20}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 25 + 15}px,${y / 25 - 0}px,0)`
+const trans3 = (x, y) => `translate3d(${x / 30 - 25}px,${y / 20 - 0}px,0)`
+
 function Home() {
+  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
+
   return (
     <div className="Home">
+      <div>
+        <div class="container" style={{marginLeft: "0", paddingTop: "100px"}}
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+
+          <div className="Left" style={{width: "50%"}}>
+            <animated.div className="image1" style={{ transform: props.xy.interpolate(trans1) }} >
+              <img src={astronaut} style={{width: "300px"}} />
+            </animated.div>
+            <animated.div className=" image2" style={{ transform: props.xy.interpolate(trans3) }}>
+              <img src={laptop} style={{width: "350px", margin: "-80px 0 0 326px"}}/>
+            </animated.div>
+          </div>
+
+        </div>
+      </div>
 
       {/* INTRO **********************/}
       <section id='intro'>
         <div className="title-info">
           <Fade duration={3000}>
-            <img id="astronaut-img" src={astronaut} />
+            <img id="astronaut-img" src={astronaut_laptop} />
             
             <div>
               <h1 id="venushacks-title">
@@ -74,7 +100,7 @@ function Home() {
       <section id='faq'>
         <Fade duration={1000}>
           <img src={faq_title}></img>
-          <FAQs></FAQs>
+          <FAQs/>
           <p style={{color: 'white', fontSize: '20px', margin: '30px 0 65px 0'}}>
             Additional logistic questions? Contact us <a href="mailto:venushacks.uci@gmail.com" target="_top">venushacks.uci@gmail.com</a>.
           </p>
