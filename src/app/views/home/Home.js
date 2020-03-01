68cb554fc3fc9a947105b51ff8e898fb352ad6fc
Alyssa Darjuan <adarjuan97@gmail.com>
2020-03-01 10:04:01 -0800

    fix parallax spacing issues

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import './Home.scss';
@@ -6,8 +7,6 @@ import './Home.scss';
 import FAQs from '../../components/faqs/FAQs';
 import Team from '../../components/meet-team/team';
 
-import venushacks_logo from 'assets/images/logo_solid.png';
-import astronaut_laptop from 'assets/images/astronaut_with_laptop.png';
 import astronaut from 'assets/images/astronaut_solid.png';
 import laptop from 'assets/images/laptop.png';
 
@@ -19,6 +18,7 @@ import sponsors_title from 'assets/images/titles/sponsors-cropped.png';
 import meet_team_title from 'assets/images/titles/meettheteam-cropped.png';
 
 // PARALLAX ASSETS
+import starsBackground from 'assets/images/stars-background.svg';
 import periwinkle_planet from 'assets/images/periwinkleplanet.png';
 import pink_planet from 'assets/images/pinkplanet.png';
 import constellation1 from 'assets/images/constellation1.png';
@@ -36,8 +36,6 @@ import oracle from 'assets/images/sponsors/oracle.png';
 import balsamiq from 'assets/images/sponsors/balsamiq1.png';
 import corelogic from 'assets/images/sponsors/corelogic.jpg';
 
-const url = (name, wrap = false) => 
-`${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 export default class Home extends React.Component {
 
@@ -48,8 +46,11 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-    this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
+
+    setTimeout(() => {
+      this.updateWindowDimensions()
+    }, 100);
   }
   
   componentWillUnmount() {
@@ -57,7 +58,11 @@ export default class Home extends React.Component {
   }
   
   updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    let contentHeight = 0
+    if (this._element && this._element.clientHeight) {
+      contentHeight = this._element.clientHeight/window.innerHeight
+    }
+    this.setState({ contentHeight: contentHeight });
   }
 
   render() {
@@ -65,13 +70,12 @@ export default class Home extends React.Component {
       <div className="Home">
 
         <Parallax 
-          ref={ref => (this.parallax = ref)} 
-          pages={this.state.width <= 500 ? 8.2 : (this.state.width > 1300 ? 6.5 : 7.2)}
+          ref={ref => (this.parallax = ref)}
+          pages={this.state.contentHeight? 2+this.state.contentHeight:8}
         >
 
-          {/* <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#253237' }} /> */}
-          <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
-          <ParallaxLayer offset={4} speed={0} factor={4} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
+          <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: `url(${starsBackground})`, backgroundSize: 'cover' }} />
+          <ParallaxLayer offset={4} speed={0} factor={4} style={{ backgroundImage: `url(${starsBackground})`, backgroundSize: 'cover' }} />
 
           {/* INTRO **********************/}
           <ParallaxLayer id="intro" offset={0} speed={0.1}>
@@ -82,7 +86,6 @@ export default class Home extends React.Component {
               </div>
 
                 <div id="intro-info-wrapper">
-                    {/* <h1 id="venushacks-title">VENUSHACKS</h1> */}
                     <img src={vh_title} style={{width: "95%", marginBottom: "15px"}}></img>
                     <h4 id="date">May 23-24, 2020</h4>
                     <p id="tagline">UC Irvine's first women-centric* hackathon</p>
@@ -142,87 +145,77 @@ export default class Home extends React.Component {
             </div>
           </ParallaxLayer>
 
-          {/* FAQ ************************/}
           <ParallaxLayer
-            id="faq"
             offset={2}
-            factor={this.state.width <= 650 ? 1.5 : 1}
             speed={0.1}
           >
-            <img src={faq_title}></img>
-            <FAQs></FAQs>
-            <p className="faq-contact-us">
-              Additional logistic questions? Contact us&nbsp;
-              <a href="mailto:venushacks.uci@gmail.com" target="_top">
-                venushacks.uci@gmail.com
-              </a>.
-            </p>
-          </ParallaxLayer>
-
-          {/* SPONSORS **********************/}
-          <ParallaxLayer
-            id="sponsors"
-            offset={this.state.width <= 650 ? 3.35 : 3}
-            factor={this.state.width <= 650 ? 1.5 : 1}
-            speed={0.1}
-          >
-              <img className="title" src={sponsors_title}></img>
-              <div id="sponsors-white-box">
-                <div className="logo large">
-                  <a href="https://www.ics.uci.edu/" target="_blank" rel="noopener noreferrer">
-                    <img src={ucibren}/>
-                  </a>
-                  <a href="https://jobs.disneycareers.com/technology" target="_blank" rel="noopener noreferrer">
-                    <img src={disney}/>
-                  </a>
-                  <a href="https://www.informatics.uci.edu/" target="_blank" rel="noopener noreferrer">
-                    <img src={informatics} style={{background: '#002143', padding: '15px 8px'}} />
-                  </a>
-                </div>
-                <div className="logo medium">
-                  <a href="https://balsamiq.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={balsamiq}/>
-                  </a>
-                  <a href="https://www.crowdstrike.com/careers/university-interns/" target="_blank" rel="noopener noreferrer">
-                    <img src={crowdstrike} />
-                  </a>
-                  <a href="https://www.oracle.com/corporate/careers/students-grads/" target="_blank" rel="noopener noreferrer">
-                    <img src={oracle}/>
-                  </a>
-                  <a href="https://www.corelogic.com/about-us/internships.aspx" target="_blank" rel="noopener noreferrer">
-                    <img id="corelogic" src={corelogic} />
-                  </a>
-                  <a href="https://careers.google.com/students/" target="_blank" rel="noopener noreferrer">
-                    <img id="google" src={google} />
-                  </a>
-                </div>
-                <div className="more-to-come-text">
-                  <span className="plus">+</span>
-                  <span style={{verticalAlign: "text-top"}}>
-                    more to come!
-                  </span>
-                </div>
+            <div ref={ref => {this._element = ref}}>
+              {/* FAQ ************************/}
+              <div id="faq">
+                <img src={faq_title}></img>
+                <FAQs></FAQs>
+                <p className="faq-contact-us">
+                  Additional logistic questions? Contact us&nbsp;
+                  <a href="mailto:venushacks.uci@gmail.com" target="_top">
+                    venushacks.uci@gmail.com
+                  </a>.
+                </p>
               </div>
-              <p className="sponsor-contact-us">
-                Join our movement, contact us&nbsp;
-                <a href="mailto:venushacks.corporate@gmail.com" target="_top">
-                  venushacks.corporate@gmail.com
-                </a>.
-              </p>
-          </ParallaxLayer>
 
-          {/* MEET THE TEAM ******************/}
-          <ParallaxLayer
-            id="meet-team"
-            offset={this.state.width <= 650 ? 4.5 : 4}
-            factor={2}
-            speed={0.1}
-            style={{height: 'auto'}}
-          >
-            <img src={meet_team_title}></img>
-            <Team></Team>
-          </ParallaxLayer>
+              {/* SPONSORS **********************/}
+              <div id="sponsors">
+                  <img className="title" src={sponsors_title}></img>
+                  <div id="sponsors-white-box">
+                    <div className="logo large">
+                      <a href="https://www.ics.uci.edu/" target="_blank" rel="noopener noreferrer">
+                        <img src={ucibren}/>
+                      </a>
+                      <a href="https://jobs.disneycareers.com/technology" target="_blank" rel="noopener noreferrer">
+                        <img src={disney}/>
+                      </a>
+                      <a href="https://www.informatics.uci.edu/" target="_blank" rel="noopener noreferrer">
+                        <img src={informatics} style={{background: '#002143', padding: '15px 8px'}} />
+                      </a>
+                    </div>
+                    <div className="logo medium">
+                      <a href="https://balsamiq.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={balsamiq}/>
+                      </a>
+                      <a href="https://www.crowdstrike.com/careers/university-interns/" target="_blank" rel="noopener noreferrer">
+                        <img src={crowdstrike} />
+                      </a>
+                      <a href="https://www.oracle.com/corporate/careers/students-grads/" target="_blank" rel="noopener noreferrer">
+                        <img src={oracle}/>
+                      </a>
+                      <a href="https://www.corelogic.com/about-us/internships.aspx" target="_blank" rel="noopener noreferrer">
+                        <img id="corelogic" src={corelogic} />
+                      </a>
+                      <a href="https://careers.google.com/students/" target="_blank" rel="noopener noreferrer">
+                        <img id="google" src={google} />
+                      </a>
+                    </div>
+                    <div className="more-to-come-text">
+                      <span className="plus">+</span>
+                      <span style={{verticalAlign: "text-top"}}>
+                        more to come!
+                      </span>
+                    </div>
+                  </div>
+                  <p className="sponsor-contact-us">
+                    Join our movement, contact us&nbsp;
+                    <a href="mailto:venushacks.corporate@gmail.com" target="_top">
+                      venushacks.corporate@gmail.com
+                    </a>.
+                  </p>
+              </div>
 
+              {/* MEET THE TEAM ******************/}
+              <div id="meet-team" style={{height: 'auto'}}>
+                <img src={meet_team_title}></img>
+                <Team></Team>
+              </div>
+            </div>
+          </ParallaxLayer>
         </Parallax>
       </div>
     )
