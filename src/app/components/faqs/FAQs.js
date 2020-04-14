81b5ac597ec31ecb643577497d3e56807f30453a
Alyssa Darjuan <adarjuan97@gmail.com>
2020-04-14 12:37:03 -0700

    added linode, refined overall styling and cleaned up code

--- src/app/components/faqs/FAQs.js
+++ src/app/components/faqs/FAQs.js
@@ -3,51 +3,49 @@ import faq_data from './FAQs.json';
 import { Card, Accordion } from 'react-bootstrap';
 import './FAQs.scss';
 
-const OneFAQ = ({index, data}) => (
-    // <div className="card-wrapper">
-         <Card className="card-wrapper">
-            <Accordion.Toggle as={Card.Header} eventKey={index}>
-                {data.question}
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey={index}>
+const SingleFAQ = ({index, data}) => (
+    <Card className="card-wrapper">
+        <Accordion.Toggle as={Card.Header} eventKey={index}>
+            {data.question}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={index}>
             <Card.Body>{data.answer}</Card.Body>
-            </Accordion.Collapse>
-        </Card>     
-    // </div> 
+        </Accordion.Collapse>
+    </Card>
 ); 
 
 export default class FAQs extends React.Component {
 
     render() {
-        var leftside = faq_data.filter(function(faq, index) {
+        var leftFAQs = faq_data.filter(function(faq, index) {
             return (index % 2 === 0);
         });
 
-        var rightside = faq_data.filter(function(faq, index) {
+        var rightFAQs = faq_data.filter(function(faq, index) {
             return (index % 2 !== 0);
         });
 
         return (
             <div id="faq-accordion-wrapper">
-                {/* Two accordions, one on each side can be open at a time */}
+                {/* Two accordions, one faq on each side can be open at a time */}
                 <div id="faq-accordion-grid">
                     <Accordion>
-                        {leftside.map((faq, i) => (
-                            <OneFAQ key={i} data={faq} index={i}/>
+                        {leftFAQs.map((faq, i) => (
+                            <SingleFAQ key={i} data={faq} index={i}/>
                         ))}
                     </Accordion>
                     <Accordion>
-                        {rightside.map((faq, i) => (
-                            <OneFAQ key={i} data={faq} index={i}/>
+                        {rightFAQs.map((faq, i) => (
+                            <SingleFAQ key={i} data={faq} index={i}/>
                         ))}
                     </Accordion>
                 </div>
 
-                {/* One accordion, one can be open at a time */}
+                {/* One accordion, one faq can be open at a time */}
                 <div id="faq-accordion-mobile">
                     <Accordion>
                         {faq_data.map((faq, i) => (
-                            <OneFAQ key={i} data={faq} index={i}/>
+                            <SingleFAQ key={i} data={faq} index={i}/>
                         ))}
                     </Accordion>
                 </div>
