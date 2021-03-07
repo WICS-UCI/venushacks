7e1bfec0de92ba2a3d2157957d265a3c95d9303d
aphan314 <alicep3@uci.edu>
2021-03-07 03:50:00 -0800

    revert master

--- src/app/components/meet-team/team.js
+++ src/app/components/meet-team/team.js
@@ -12,21 +12,8 @@ export default class Team extends React.Component {
             {photo: 'christian', name: "Christian Chun"},
             {photo: 'danielle', name: "Danielle Muhlenberg"},
             {photo: 'deepal', name: "Deepal Sanghrajka"},
-<<<<<<< HEAD
-<<<<<<< HEAD
             {photo: 'jp', name: "JP Nguyen"},
             {photo: 'kaeley', name: "Kaeley Lenard"},
-=======
-            {photo: 'elise', name: "Elise Jang"},
-            {photo: 'ellen', name: "Ellen Kim"},
-            {photo: 'ellise', name: "Ellise Limjoco"},
-            {photo: 'karen', name: "Karen Vu"},
-            {photo: 'kasey', name: "Kasey Chuang"},
->>>>>>> parent of b979899... Update team.js
-=======
-            {photo: 'jp', name: "JP Nguyen"},
-            {photo: 'kaeley', name: "Kaeley Lenard"},
->>>>>>> parent of b0424ff... hack at uci + org photos
             {photo: 'kayla', name: "Kayla Tran"},
             {photo: 'karen', name: "Karen Vu"},
             {photo: 'megha', name: "Megha Kak"},
