97f3e213c06630fe498ffa5b8f263b02fb0ffd89
aphan314 <alicep3@uci.edu>
2021-03-07 03:41:32 -0800

    Revert "Update team.js"
    This reverts commit b9798992010bc511a715cc24785ada165a1c937b.

--- src/app/components/meet-team/team.js
+++ src/app/components/meet-team/team.js
@@ -5,34 +5,44 @@ export default class Team extends React.Component {
     render() {
         let organizers = [
             {photo: "alice", name: "Alice Phan"},
-            {photo: "ameya", name: "Ameya Mandalik"},
-            {photo: 'april', name: "April Lee"},
+            {photo: "alyssa", name: "Alyssa Darjuan"},
+            {photo: 'archita', name: "Archita Ganesh"},
             {photo: 'areeta', name: "Areeta Wong"},
-            {photo: 'bhavani', name: "Bhavani Panda"},
-            {photo: 'chase', name: "Chase Carnaroli"},
-            {photo: 'crystal', name: "Crystal Lee"},
+            {photo: 'chey', name: "Chey Chavez"},
+            {photo: 'christian', name: "Christian Chun"},
+            {photo: 'danielle', name: "Danielle Muhlenberg"},
             {photo: 'deepal', name: "Deepal Sanghrajka"},
+<<<<<<< HEAD
+            {photo: 'jp', name: "JP Nguyen"},
+            {photo: 'kaeley', name: "Kaeley Lenard"},
+=======
             {photo: 'elise', name: "Elise Jang"},
             {photo: 'ellen', name: "Ellen Kim"},
             {photo: 'ellise', name: "Ellise Limjoco"},
+            {photo: 'karen', name: "Karen Vu"},
             {photo: 'kasey', name: "Kasey Chuang"},
+>>>>>>> parent of b979899... Update team.js
             {photo: 'kayla', name: "Kayla Tran"},
-            {photo: 'khushi', name: "Khushi Valia"},
-            {photo: 'kingsley', name: "Kingsley Szeto"},
+            {photo: 'karen', name: "Karen Vu"},
             {photo: 'megha', name: "Megha Kak"},
-            {photo: 'nicole', name: "Nicole Pham"},
-            {photo: 'rieko', name: "Rieko Konishi"},
-            {photo: 'riley', name: "Riley Champion"},
+            {photo: 'mohra', name: "Mohra Arsala"},
+            {photo: 'nehal', name: "Nehal Desai"},
+            {photo: 'paul', name: "Paul Yang"},
+            {photo: 'sahil', name: "Sahil Railkar"},
+            {photo: 'sandy', name: "Sandy Pan"},
             {photo: 'shannon', name: "Shannon Hoang"},
+            {photo: 'sharon', name: "Sharon Hsu"},
+            {photo: 'shreyas', name: "Shreyas Hukkeri"},
+            {photo: 'tedrick', name: "Tedrick Wong"},
+            {photo: 'theja', name: "Theja Krishna"},
+            {photo: 'tiffany', name: "Tiffany Liang"},
+            {photo: 'zach', name: "Zach Pinto"},
         ];
 
         const images = organizers.map((organizer, i) =>
             <div key={i} style={{display:"inline-block" }}>
                 <div className="organizer-img-wrapper">
-                    <img src = {require('assets/images/organizer-photos/'+organizer.photo+'.jpeg')}
-                    onmouseover="this.src='./assets/images/organizer-photos-funny/'+organizer.photo+'-funny.jpeg'"
-                    onmouseout="this.src='./assets/images/organizer-photos/'+organizer.photo+'.jpeg'"
-                    />
+                    <img src = {require('assets/images/organizer-photos/'+organizer.photo+'.jpeg')}/>
                 </div>
                 <p>{organizer.name}</p>
             </div>
--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -134,16 +134,9 @@ export default class Home extends React.Component {
                   <img id="venushacks-title" src={vh_title} />
                   <h4 id="date">April 24 - 25, 2021</h4>
                   <p id="tagline">UC Irvine's first women-centric* hackathon</p>
-                    <Button id="apply-btn" disabled variant='light'>
-                      <a href="https://airtable.com/shrCdPWeLmyfxrKvT">
-                        HACKER APP
-                      </a>
-                    </Button>
-                    <Button id="apply-btn" disabled variant='light'>
-                      <a href="https://airtable.com/shrleZ2rcEiyrnE0f">
-                        MENTOR APP
-                      </a>
-                    </Button>
+                  <Button id="apply-btn" disabled variant='light'>
+                    Applications open TBD
+                  </Button>
                 </div>
 
             </section>
@@ -184,7 +177,7 @@ export default class Home extends React.Component {
                 <p>
                   Planned in collaboration with&nbsp;
                   <a href="https://wics.ics.uci.edu/" target="_blank" rel="noopener noreferrer">WICS</a> and&nbsp;
-                  <a href="https://www.hackuci.com/" target="_blank" rel="noopener noreferrer">Hack at UCI</a>,&nbsp;
+                  <a href="https://www.hackuci.com/" target="_blank" rel="noopener noreferrer">Hack</a>,&nbsp;
                   VenusHacks will be UCI’s 
                   first women-centric* hackathon and will take place all online! 
                 </p>
