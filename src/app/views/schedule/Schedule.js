2e3e95440b84e7b5a13f501d732875e1496ee4c4
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-04-18 23:16:53 -0700

    Make Schedule and Start Packs titles more responsive

--- src/app/views/schedule/Schedule.js
+++ src/app/views/schedule/Schedule.js
@@ -9,22 +9,15 @@ function Schedule() {
     <div className="Schedule">
       <section id="schedule-block">
         <div className="schedule-countdown">
-          <div>
-            <div>
-              <FloatingHat offset={0}></FloatingHat>
-            </div>
-            <h3>
-              Schedule
-            </h3>
-            <div>
-              <FloatingHat offset={2}></FloatingHat>
-            </div>
-          </div>
-          <div className="schedule-countdown-border"></div>
+          <FloatingHat offset={0}></FloatingHat>
+          <h2>
+            Schedule
+          </h2>
+            <FloatingHat offset={2}></FloatingHat>
         </div>
         <ScheduleCard/>
       </section>
-      </div>
+    </div>
   );
 }
 
