61234b4e98e2cd6f67c0746bb57f2b4b3ed75de0
ellenkim <kim.ellen9@gmail.com>
2021-04-20 19:14:05 -0700

    descriptions tooltip added and format changed

--- src/app/components/schedule-card/ScheduleCard.js
+++ src/app/components/schedule-card/ScheduleCard.js
@@ -1,142 +1,8 @@
 import React from 'react';
 import './ScheduleCard.scss';
 
-const satSchedule = [
-  {
-    name: 'Team Formation',
-    link: 'https://uci.zoom.us/j/92221155593',
-    description: 'Meet fellow hackers!',
-    time: '9:00AM-10:00AM'
-  },
-  {
-    name: 'Opening Ceremony',
-    link: 'https://youtu.be/YOVul8y9tUw',
-    description: 'Getting started',
-    time: '10:00AM-11:00AM'
-  },
-  {
-    name: 'Tech for Good Panel',
-    link: 'https://uci.zoom.us/j/94535498642',
-    description: 'Change.org, LaunchCode, Nina Space',
-    time: '11:00AM-12:00PM'
-  },
-  {
-    name: 'Hacking Begins',
-    description: 'Blast-off!',
-    time: '12:00PM'
-  },
-  {
-    name: 'Intro to Web Dev',
-    link: 'https://uci.zoom.us/j/98907500070',
-    description: 'WICS',
-    time: '12:00PM-1:00PM'
-  },
-  {
-    name: 'iOS Development',
-    link: 'https://uci.zoom.us/j/98043160884',
-    description: 'CodePath',
-    time: '12:00PM-1:00PM'
-  },
-  {
-    name: 'Intro to UI/UX Design',
-    link: 'https://uci.zoom.us/j/96828624934',
-    description: 'Design at UCI',
-    time: '1:00PM-2:00PM'
-  },
-  {
-    name: 'Disney Tech Talk',
-    link: 'https://uci.zoom.us/j/99473643316',
-    description: 'Disney',
-    time: '2:00PM-3:00PM'
-  },
-  {
-    name: 'Android Development',
-    link: 'https://uci.zoom.us/j/92109394679',
-    description: 'CodePath',
-    time: '3:00PM-4:00PM'
-  },
-  {
-    name: 'APIs 101 Workshop',
-    link: 'https://uci.zoom.us/j/92479898700',
-    description: 'Postman',
-    time: '4:00PM-5:00PM'
-  },
-  {
-    name: 'The Unity Game Engine',
-    link: 'https://uci.zoom.us/j/98094631168',
-    description: 'VGDC',
-    time: '5:00PM-6:00PM'
-  },
-  {
-    name: 'Cyber Security Challenge',
-    link: 'https://uci.zoom.us/j/99122491205',
-    description: 'MLH',
-    time: '5:30PM-6:00PM'
-  },
-  {
-    name: 'GraphQL Workshop',
-    link: 'https://uci.zoom.us/j/96242131335',
-    description: 'ICSSC',
-    time: '6:00PM-7:00PM'
-  },
-  {
-    name: 'Supervised Learning with Online Datasets Workshop',
-    link: 'https://uci.zoom.us/j/99364847238',
-    description: 'AI@UCI',
-    time: '7:00PM-8:00PM'
-  },
-  {
-    name: 'Imposter Syndrome',
-    link: 'https://uci.zoom.us/j/95912998559',
-    description: 'SWE',
-    time: '8:00PM-9:00PM'
-  },
-  {
-    name: 'Drawphone',
-    link: 'https://uci.zoom.us/j/98223072689',
-    description: 'Let\'s have some fun!',
-    time: '9:00PM-10:00PM'
-  },
-  {
-    name: 'How to Pitch Your Hackathon Project',
-    link: 'https://uci.zoom.us/j/94401407167',
-    description: 'MAISS',
-    time: '10:00PM-11:00PM'
-  },
-  {
-    name: 'binarysearch',
-    link: 'https://uci.zoom.us/j/95759746304',
-    description: 'Let\'s do some coding!',
-    time: '11:00PM-12:00AM'
-  },
-]
-
-const sunSchedule = [
-  {
-    name: 'Hacking Ends',
-    description: 'Landing!',
-    time: '12:00PM'
-  },
-  {
-    name: 'Diversity in Tech Panel',
-    link: 'https://uci.zoom.us/j/91396656865',
-    description: 'Linode, Darktrace, Procure Technologies',
-    time: '2:00PM-3:00PM'
-  },
-  {
-    name: 'Bob Ross Painting Along',
-    link: 'https://uci.zoom.us/j/94228741813',
-    description: 'Let\'s get creative!',
-    time: '5:30PM-6:00PM'
-  },
-  {
-    name: 'Closing Ceremony',
-    link: 'https://youtu.be/koFpykh3hrA',
-    description: 'See you next year!',
-    time: '6:00PM-7:00PM'
-  },
-  
-]
+import { satScheduleData } from "assets/data/sat-schedule-info.js";
+import { sunScheduleData } from "assets/data/sun-schedule-info.js";
 
 function ScheduleCard() {
   return (
@@ -144,14 +10,20 @@ function ScheduleCard() {
       <div className="schedule-card-section">
         <div>
           <h4>Saturday</h4>
-          {satSchedule.map(function(event, index) {
+          <div className="schedule-card-divider-horizontal">
+            <div/>
+          </div>
+          {satScheduleData.map(function(event, index) {
             return(
               <div className="schedule-card-time-slot" key={index}>
                 <div>
                   <a href={event.link}  style={{'text-decoration': 'none'}}> 
                     <h5> {event.name}</h5>
                   </a>
-                  <p>{event.description}</p>
+                  <div class="schedule-card-tooltip">
+                    <p>{event.description}</p>
+                    <span class="schedule-card-tooltiptext">{event.tooltip}</span>
+                  </div>
                 </div>
                 <div style={{'textAlign': 'right'}}>
                   <h5>{event.time}</h5>
@@ -161,16 +33,13 @@ function ScheduleCard() {
           })}
         </div>
       </div>
-      <div className="schedule-card-divider-vertical">
-        <div/>
-      </div>
-      <div className="schedule-card-divider-horizontal">
-        <div/>
-      </div>
       <div className="schedule-card-section">
         <div>
           <h4>Sunday</h4>
-          {sunSchedule.map(function(event, index) {
+          <div className="schedule-card-divider-horizontal">
+            <div/>
+          </div>
+          {sunScheduleData.map(function(event, index) {
             return(
               <div className="schedule-card-time-slot" key={index}>
                 <div>
---
+++ src/assets/data/sun-schedule-info.js
@@ -0,0 +1,32 @@
+const sunScheduleData = [
+    {
+      name: 'Hacking Ends',
+      description: 'Landing!',
+      tooltip: "Hacking officially ends",
+      time: '12:00PM'
+    },
+    {
+      name: 'Diversity in Tech Panel',
+      link: 'https://uci.zoom.us/j/91396656865',
+      description: 'Linode, Darktrace, Procure Technologies',
+      tooltip: "Come join us for our Diversity in Tech Panel with a diverse set of empowering women from Linode, Darktrace, and Procure Technologies. We will be listening to how their identity as women impacted their personal journeys, decisions, and who they are today.",
+      time: '2:00PM-3:00PM'
+    },
+    {
+      name: 'Bob Ross Painting Along',
+      link: 'https://uci.zoom.us/j/94228741813',
+      description: 'Let\'s get creative!',
+      tooltip: "At Bob Ross MS Paint, you'll relax as you recreate your own Bob Ross painting in MS Paint.",
+      time: '5:30PM-6:00PM'
+    },
+    {
+      name: 'Closing Ceremony',
+      link: 'https://youtu.be/koFpykh3hrA',
+      description: 'See you next year!',
+      tooltip: "Ready to find out if you won a prize? Then come out to our closing ceremony where we will announce all the winners for VenusHacks 2021! We will also hear from our closing keynote speaker, Emily Van Norden, the Senior Director of DE&I and Corporate Communications at Crowdstrike!",
+      time: '6:00PM-7:00PM'
+    },
+    
+  ]
+
+export { sunScheduleData };
