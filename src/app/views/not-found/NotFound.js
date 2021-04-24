77bc133f2890b5cc1071884eb1e2001cac8b8a5d
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-04-24 02:07:54 -0700

    Add stars, add footer

--- src/app/views/not-found/NotFound.js
+++ src/app/views/not-found/NotFound.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Footer } from "app/components";
 
 import "./NotFound.scss";
 
 function NotFound() {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <span className="h2">Not Found</span>
-      <span>Just empty space over here.</span>
-      {/* no stars */}
+      <div className="not-found-message">
+        <h1>404</h1>
+        <span className="h2">Not Found</span>
+        <span>Just empty space over here.</span>
+      </div>
+      <Footer />
     </div>
   );
 }
