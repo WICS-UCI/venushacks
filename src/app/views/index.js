99906c540e0209690f20060c6c5b8e4c727fd9dc
Taesung Hwang <44419552+taesungh@users.noreply.github.com>
2021-04-24 01:57:18 -0700

    created 404 not found page (#102)

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './home/Home';
 export { default as Schedule } from './schedule/Schedule';
 export { default as StarterPacks } from './starter-packs/StarterPacks';
 export { default as Workshops } from './workshops/Workshops';
+export { default as NotFound } from './not-found/NotFound';
---
+++ src/app/views/not-found/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./NotFound.scss";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <span className="h2">Not Found</span>
+      <span>Just empty space over here.</span>
+      {/* no stars */}
+    </div>
+  );
+}
+
+export default NotFound;
