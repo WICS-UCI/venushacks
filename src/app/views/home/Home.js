f9da282a56bbcb2d54e6604d5df18b2cc71f1ad5
Nathan <nathan.toan.ng@gmail.com>
2022-03-16 13:26:04 -0700

    Created Mentor App Button and Applied Button Hovering

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -158,9 +158,14 @@ export default class Home extends React.Component {
                   <img id="venushacks-title" src={vh_title} alt="VenusHacks Title Logo"/>
                   <h4 id="date">May 20 - 22, 2022</h4>
                   <p id="tagline">UC Irvine's largest women-centric hackathon</p>
-                    <Button id="apply-btn">
+                    <Button id="apply-hacker-btn">
                       <a href="https://airtable.com/shr1Etr4Typ3FEqBc" target="_blank" rel="noopener noreferrer">
                         Apply as a Hacker
+                      </a>
+                    </Button>
+                    <Button id="apply-mentor-btn">
+                      <a href="https://airtable.com/shr5Y9x5ocJaJL52j" target="_blank" rel="noopener noreferrer">
+                        Apply as a Mentor
                       </a>
                     </Button>
                 </div>
