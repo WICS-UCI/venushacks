e53705a0e7dc27b2512cd6f83a37c9e47e449c45
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-04-12 10:55:21 -0700

    Update Instagram

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
             <a href="https://www.facebook.com/venushacksUCI/" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-facebook"></i>
             </a>
-            <a href="https://www.instagram.com/venushacks_uci/?igshid=15yqwb3mmui1u" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/venushacksuci" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-instagram"></i>
             </a>
         </footer>
