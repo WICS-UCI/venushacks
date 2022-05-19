f0b2f7efced7241e00c6203f6238f32b361064ee
Nathan Nguyen <Nathan.toan.ng@gmail.com>
2022-05-19 00:47:25 -0700

    Fixed indentation and utilized class for location

--- src/app/components/schedule-card/ScheduleCard.js
+++ src/app/components/schedule-card/ScheduleCard.js
@@ -4,7 +4,7 @@ import './ScheduleCard.scss';
 import { scheduleData } from "assets/data/schedule-info.js";
 
 function ScheduleCard() {
-  const {thurSchedule, friSchedule, satSchedule, sunSchedule} = scheduleData;
+  const { thurSchedule, friSchedule, satSchedule, sunSchedule } = scheduleData;
 
   return (
     <div className="schedule-card">
@@ -12,24 +12,24 @@ function ScheduleCard() {
         <div>
           <h4>Thursday</h4>
           <div className="schedule-card-divider-horizontal">
-            <div/>
+            <div />
           </div>
-          {thurSchedule.map(function(event, index) {
-            return(
+          {thurSchedule.map(function (event, index) {
+            return (
               <div className="schedule-card-time-slot" key={index}>
                 <div>
-                    <h5> {event.name}</h5>
+                  <h5> {event.name}</h5>
                   <div class="schedule-card-location">
-                  <a href={event.link}  style={{'text-decoration': 'none'}}>
-                  <p>{event.location}</p>
-                  </a>
-                    </div>
+                    <a href={event.link} >
+                      <p>{event.location}</p>
+                    </a>
+                  </div>
                   <div class="schedule-card-tooltip">
                     <p>{event.description}</p>
                     <span class="schedule-card-tooltiptext">{event.tooltip}</span>
                   </div>
                 </div>
-                <div style={{'textAlign': 'right'}}>
+                <div style={{ 'textAlign': 'right' }}>
                   <h5>{event.time}</h5>
                 </div>
               </div>
@@ -41,24 +41,24 @@ function ScheduleCard() {
         <div>
           <h4>Friday</h4>
           <div className="schedule-card-divider-horizontal">
-            <div/>
+            <div />
           </div>
-          {friSchedule.map(function(event, index) {
-            return(
+          {friSchedule.map(function (event, index) {
+            return (
               <div className="schedule-card-time-slot" key={index}>
                 <div>
-                    <h5> {event.name}</h5>
+                  <h5> {event.name}</h5>
                   <div class="schedule-card-location">
-                  <a href={event.link}  style={{'text-decoration': 'none'}}>
-                  <p>{event.location}</p>
-                  </a>
-                    </div>
+                    <a href={event.link} >
+                      <p>{event.location}</p>
+                    </a>
+                  </div>
                   <div class="schedule-card-tooltip">
                     <p>{event.description}</p>
                     <span class="schedule-card-tooltiptext">{event.tooltip}</span>
                   </div>
                 </div>
-                <div style={{'textAlign': 'right'}}>
+                <div style={{ 'textAlign': 'right' }}>
                   <h5>{event.time}</h5>
                 </div>
               </div>
@@ -70,24 +70,24 @@ function ScheduleCard() {
         <div>
           <h4>Saturday</h4>
           <div className="schedule-card-divider-horizontal">
-            <div/>
+            <div />
           </div>
-          {satSchedule.map(function(event, index) {
-            return(
+          {satSchedule.map(function (event, index) {
+            return (
               <div className="schedule-card-time-slot" key={index}>
                 <div>
-                    <h5> {event.name}</h5>
+                  <h5> {event.name}</h5>
                   <div class="schedule-card-location">
-                  <a href={event.link}  style={{'text-decoration': 'none'}}>
-                  <p>{event.location}</p>
-                  </a>
-                    </div>
+                    <a href={event.link} >
+                      <p>{event.location}</p>
+                    </a>
+                  </div>
                   <div class="schedule-card-tooltip">
                     <p>{event.description}</p>
                     <span class="schedule-card-tooltiptext">{event.tooltip}</span>
                   </div>
                 </div>
-                <div style={{'textAlign': 'right'}}>
+                <div style={{ 'textAlign': 'right' }}>
                   <h5>{event.time}</h5>
                 </div>
               </div>
@@ -99,24 +99,24 @@ function ScheduleCard() {
         <div>
           <h4>Sunday</h4>
           <div className="schedule-card-divider-horizontal">
-            <div/>
+            <div />
           </div>
-          {sunSchedule.map(function(event, index) {
-            return(
+          {sunSchedule.map(function (event, index) {
+            return (
               <div className="schedule-card-time-slot" key={index}>
                 <div>
-                    <h5> {event.name}</h5>
+                  <h5> {event.name}</h5>
                   <div class="schedule-card-location">
-                  <a href={event.link}  style={{'text-decoration': 'none'}}>
-                  <p>{event.location}</p>
-                  </a>
-                    </div>
+                    <a href={event.link} >
+                      <p>{event.location}</p>
+                    </a>
+                  </div>
                   <div class="schedule-card-tooltip">
                     <p>{event.description}</p>
                     <span class="schedule-card-tooltiptext">{event.tooltip}</span>
                   </div>
                 </div>
-                <div style={{'textAlign': 'right'}}>
+                <div style={{ 'textAlign': 'right' }}>
                   <h5>{event.time}</h5>
                 </div>
               </div>
