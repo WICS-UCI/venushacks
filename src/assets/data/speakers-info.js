ea737607e5cf5ed24158ae472dc4ed1219313c21
Grace Manning <gracebmanning@gmail.com>
2022-05-19 18:46:19 -0700

    update speakers section

--- src/assets/data/speakers-info.js
+++ src/assets/data/speakers-info.js
@@ -1,17 +1,11 @@
 let speakersData = {
-    keynotes: [
-        {photo: "lisa", name: "Lisa Conn", title: "Cofounder & Chief Operating Officer at Icebreaker"},
-        {photo: "emily", name: "Emily Van Norden", title: "Senior Director of Diversity, Equity, & Inclusion and Corporate Communications at Crowdstrike"},
-    ],
     panelists: [
-        {photo: "maira", name: "Maira Benjamin", title: "Senior Director of Engineering at Change.org"},
-        {photo: "tiffany", name: "Tiffany Wong", title: "Software Engineer at Linode"},
-        {photo: "victoria", name: "Victoria Good", title: "Lead Software Engineer at Linode"},
-        {photo: "sally", name: "Sally Steuterman", title: "Senior Education Program Developer at LaunchCode"},
-        {photo: "ekaterina", name: "Ekaterina (Katia) Stambolieva", title: "Founder of Cosmos Pics"},
-        {photo: "danielle", name: "Danielle Sandoval", title: "Group Product Manager at Procore Technologies"},
-        {photo: "kristina", name: "Kristina Harris", title: "Global Executive Engagement at DarkTrace"},
-        {photo: "areeta", name: "Areeta Wong", title: "Previous Co-President of Blueprint at UCI"},
+        {photo: "joohee", name: "Joohee Yoo", title: "Staff Data Engineer at Slack"},
+        {photo: "lauren", name: "Lauren Yoo", title: "Software Engineer at OpenFin"},
+        {photo: "jenny", name: "Jenny Mankin", title: "Principal Software Engineer at Crowdstrike"},
+    ],
+    closing: [
+        {photo: "christina", name: "Christina Hastenrath", title: "Software Engineer at Postman"},
     ]
 };
 
