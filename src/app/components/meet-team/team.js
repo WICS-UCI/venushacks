fbcd83d8b5bb2a008cac6942172540ecab6afcd4
sirAvent <chiu7a@gmail.com>
2023-05-02 14:23:55 -0700

    2023 org photos + icssc logo update

--- src/app/components/meet-team/team.js
+++ src/app/components/meet-team/team.js
@@ -5,42 +5,48 @@ import './team.scss';
 export default class Team extends React.Component {
     render() {
         let organizers = [
-            {photo: "aasha", name: "Aasha Sendhil"},
             {photo: "alan", name: "Alan Chang"},
-            {photo: "ameya", name: "Ameya Mandalik"},
-            {photo: "angela", name: "Angela Qiao"},
-            {photo: "chase", name: "Chase Carnaroli"},
+            {photo: "alicia", name: "Alicia Chuang"},
+            {photo: "alisa", name: "Alisa Liu"},
+            {photo: "araam", name: "Araam Abutaha"},
+            {photo: "audrey", name: "Audrey Lam"},
+            {photo: "avent", name: "Avent Chiu"},
+            {photo: "brian", name: "Brian Lin"},
+            {photo: "caroline", name: "Caroline Wang"},
+            {photo: "cheryl", name: "Cheryl Chen"},
             {photo: "chloe", name: "Chloe Cheng"},
-            {photo: "duong", name: "Duong Vu"},
-            {photo: "elise", name: "Elise Jang"},
-            {photo: "ellise", name: "Ellise Limjoco"},
-            {photo: "emily", name: "Emily Doan"},
+            {photo: "cinta", name: "Cinta Adhiningrat"},
+            {photo: "crystal", name: "Crystal Lai"},
             {photo: "grace", name: "Grace Manning"},
-            {photo: "joseph", name: "Joseph Gu"},
-            {photo: "julianna", name: "Julianna Cardenas"},
-            {photo: "khushi", name: "Khushi Valia"},
-            {photo: "kingsley", name: "Kingsley Szeto"},
-            {photo: "megha", name: "Megha Kak"},
-            {photo: "nathan", name: "Nathan Nguyen"},
-            {photo: "nicole", name: "Pham"},
-            {photo: "olivia", name: "Olivia Ih"},
-            {photo: "parsa", name: "Parsa Karamat"},
+            {photo: "graceW", name: "Grace Wang"},
+            {photo: "hang", name: "Hang Cao"},
+            {photo: "jefferey", name: "Jefferey Lee Chea"},
+            {photo: "jenny", name: "Jenny Liu"},
+            {photo: "kirby", name: "Kirby Ammari"},
+            {photo: "kristen", name: "Kristen Yee"},
+            {photo: "nathanC", name: "Nathan Choi"},
+            {photo: "nathan", name: "Nathan Huey"},
+            {photo: "nicole", name: "Nicole Nguyen"},
+            {photo: "mason", name: "Mason Wong"},
+            {photo: "mignon", name: "Mignon April Lee"},
+            {photo: "mirelle", name: "Mirelle George"},
             {photo: "philip", name: "Philip Truong"},
+            {photo: "randy", name: "Randy Hyunh"},
             {photo: "riley", name: "Riley Wong"},
             {photo: "rochelle", name: "Rochelle Nixon"},
-            {photo: "rong", name: "Rong Mu"},
-            {photo: "sanjana", name: "Sanjana Magidewar"},
-            {photo: "shalini", name: "Shalini Bhakta"},
-            {photo: "sydney", name: "Sydnee Tan"},
-            {photo: "william", name: "William Hou"},
-            {photo: "zoya", name: "Zoya Hajee"}
+            {photo: "rosalind", name: "Rosalind Guo"},
+            {photo: "ruslan", name: "Ruslan Manoharan"},
+            {photo: "ryan", name: "Ryan Eurich"},
+            {photo: "sam", name: "Sam Der"},
+            {photo: "shreshta", name: "Shreshta Kumar"},
+            {photo: "taylor", name: "Taylor Quach"}
         ];
 
         const images = organizers.map((organizer, i) =>
             <div key={i} style={{display:"inline-block" }}>
                 <div className="organizer-img-wrapper">
                     <img 
-                        src = {require('assets/images/organizer-photos/'+organizer.photo+'.jpeg')}
+                        src = {require('assets/images/organizer-photos/'+organizer.photo+'.jpg')}
                         alt = {organizer.photo + "'s profile picture"}
                         //onMouseOver={e => (e.currentTarget.src = require('assets/images/organizer-photos-funny/'+organizer.photo+'-funny.jpeg'))}
                         //onMouseOut={e => (e.currentTarget.src = require('assets/images/organizer-photos/'+organizer.photo+'.jpeg'))}
