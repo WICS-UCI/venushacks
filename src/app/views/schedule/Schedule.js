4f88a889fa8ca3240541b3511fca00f56c63089c
Alan Chang <alan@alanchang.xyz>
2023-05-22 23:18:45 -0700

    Remove trailing Schedule header

--- src/app/views/schedule/Schedule.js
+++ src/app/views/schedule/Schedule.js
@@ -16,7 +16,6 @@ function Schedule() {
             <FloatingHat offset={2}></FloatingHat>
         </div>
         <h4> All times are in PDT </h4>
-        {/* <h4 style={{'marginBottom': '400px'}}> Schedule coming soon! </h4> */}
         <ScheduleCard/>
       </section>
       <Footer/>
