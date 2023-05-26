bbdc006e12af97390b4abe1e45a8cc5d0b8b8719
Alan Chang <alan@alanchang.xyz>
2023-05-25 22:40:09 -0700

    Update resourceswith newest version

--- src/assets/data/resources-info.js
+++ src/assets/data/resources-info.js
@@ -4,54 +4,118 @@ let starterPackData = [
     description: "Build a frontend you can interact with!",
     packs: [
       {
-        name: "React",
-        link: "https://beta.reactjs.org/learn",
-        tooltip:
-          "Learn how to use React, a JavaScript library for building user interfaces"
+        name: "Frontend Starter Code",
+        link: "https://github.com/HackAtUCI/zothacks-frontend-startercode",
+        tooltip: "A basic webpage built using HTML, CSS, and JavaScript.",
       },
       {
-        name: "ZotHacks React Frontend",
-        link: "https://github.com/hackuci/zothacks-2020-frontend",
+        name: "React Workshop",
+        link: "https://github.com/nmpham2/hack-at-uci-react-intro",
         tooltip:
-          "Checkout the frontend project and video tutorial from ZotHacks 2020! A great first step to getting started with a React frontend."
+          "Learn how to use React, a JavaScript library for building user interfaces.",
       },
       {
-        name: "iOS App Development Workshop",
-        link: "https://www.youtube.com/watch?v=50Uxl6hz3Ug",
-        tooltip:
-          "CodePath at UCI walks you through the basics of Xcode and how to build a simple iOS app for displaying a list of items."
+        name: "React Native",
+        link: "https://reactnative.dev/",
+        tooltip: "React, but for mobile!",
       },
       {
-        name: "Android Zothacks Workshop",
-        link: "https://www.youtube.com/watch?v=u0tNBjglS2M",
+        name: "Android Studio",
+        link: "https://developer.android.com/studio",
         tooltip:
-          "CodePath at UCI teaches you how to build a simple To-Do app in Android Studio."
-      }
-    ]
+          "Need a mobile emulator? Then Android Studio is the tool for you!",
+      },
+      {
+        name: "Flutter SDK",
+        link: "https://docs.flutter.dev/",
+        tooltip: "Tackle mobile development with this modern mobile SDK!",
+      },
+    ],
   },
   {
     name: "Backend",
     description: "Build an API for data management and processing!",
     packs: [
       {
-        name: "ZotHacks Flask Backend",
-        link: "https://github.com/hackuci/zothacks-2020-backend",
-        tooltip:
-          "Checkout the backend project and video tutorial from ZotHacks 2020! A great first step to getting started with a Flask backend."
+        name: "Simple Flask Example",
+        link: "https://github.com/HackAtUCI/zothacks-backend-startercode",
+        tooltip: "A basic backend example using Flask.",
+      },
+      {
+        name: "Webscraping Workshop",
+        link: "https://github.com/will-hou/hackatuci-learn-webscraping-2023",
+        tooltip: "Learn how to extract data from websites!",
       },
       {
         name: "Free Public APIs",
         link: "https://github.com/public-apis/public-apis",
-        tooltip:
-          "A collective list of free APIs for use in software and web development."
+        tooltip: "A collective list of free APIs to use.",
       },
       {
-        name: "PeterPortal - An Unofficial API for UCI",
+        name: "FastAPI",
+        link: "https://fastapi.tiangolo.com/",
+        tooltip: "A popular API Framework for Python.",
+      },
+      {
+        name: "PeterPortal Public API",
         link: "https://api.peterportal.org/",
         tooltip:
-          "PeterPortal API provides consolidated data about UCI professors, courses, and grades."
+          "PeterPortal API provides data about UCI professors, courses, and grades.",
       },
-    ]
+    ],
+  },
+  {
+    name: "Databases",
+    description: "Store data in your own database!",
+    packs: [
+      {
+        name: "CockroachDB",
+        link: "https://www.cockroachlabs.com/docs/",
+        tooltip: "A distributed SQL database from our sponsor Cockroach Labs!",
+      },
+      {
+        name: "MongoDB",
+        link: "https://www.mongodb.com/",
+        tooltip: "A popular NoSQL database.",
+      },
+      {
+        name: "PostgreSQL",
+        link: "https://www.postgresql.org/",
+        tooltip: "A popular object-relational database.",
+      },
+      {
+        name: "MySQL",
+        link: "https://www.mysql.com/",
+        tooltip: "A popular relational database.",
+      },
+    ],
+  },
+  {
+    name: "Deployment",
+    description:
+      "Ready to deploy? Here are some tools to help you get started.",
+    packs: [
+      {
+        name: "GitHub Pages",
+        link: "https://pages.github.com/",
+        tooltip: "Deploy your static site on GitHub Pages!",
+      },
+      {
+        name: "Vercel",
+        link: "https://vercel.com/",
+        tooltip: "Deploy your frontend app easily!",
+      },
+      {
+        name: "Google Cloud Platform",
+        link: "https://cloud.google.com/docs",
+        tooltip: "Scale your project with Google Cloud!",
+      },
+      {
+        name: "Amazon Web Services",
+        link: "https://docs.aws.amazon.com",
+        tooltip: "Deploy your app to this large cloud computing platform!",
+      },
+    ],
   },
   {
     name: "Gaming",
@@ -61,52 +125,73 @@ let starterPackData = [
         name: "Unity",
         link: "https://docs.unity3d.com/Manual/index.html",
         tooltip:
-          "Official documentation for Unity, a cross-platform game engine"
+          "Official documentation for Unity, a cross-platform game engine",
       },
       {
         name: "Godot",
         link: "https://godotengine.org/",
         tooltip:
-          "Godot provides a huge set of common tools for game development"
+          "Godot provides a huge set of common tools for game development",
       },
       {
         name: "PyGame",
         link: "https://www.pygame.org/wiki/GettingStarted",
         tooltip:
-          "Official documentation for PyGame, a cross-platform set of Python modules designed for writing video games"
-      }
-    ]
+          "Official documentation for PyGame, a cross-platform set of Python modules designed for writing video games",
+      },
+    ],
   },
   {
-    name: "Tools",
+    name: "AI / Machine Learning",
+    description:
+      "Spice up your hackathon project with machine learning and AI!",
+    packs: [
+      {
+        name: "Tensorflow",
+        link: "https://www.tensorflow.org/",
+        tooltip:
+          "A comprehensive library for machine learning and artificial intelligence.",
+      },
+      {
+        name: "Pytorch",
+        link: "https://pytorch.org/",
+        tooltip: "A machine learning framework.",
+      },
+      {
+        name: "Scikit-learn",
+        link: "https://scikit-learn.org/stable/",
+        tooltip: "A data analysis and machine learning library.",
+      },
+    ],
+  },
+  {
+    name: "General Resources",
     description: "Gotta build your projects with something!",
     packs: [
       {
         name: "Git",
-        link: "https://github.com/zpinto/learn-git-with-hack",
-        tooltip:
-          "A complete tutorial on how to get started with Git, a version-control system"
+        link: "https://docs.google.com/presentation/d/1Omd_azmTLQqrIjc6Bkva7PetFeQosLpfyLox8xDA5Mg/edit",
+        tooltip: "A popular version control tool!",
       },
       {
         name: "VS Code",
         link: "https://code.visualstudio.com/",
-        tooltip: 
-          "A streamlined code editor with support for development operations like debugging, task running, and version control."
+        tooltip:
+          "A streamlined code editor with support for debugging, task running, and version control.",
+      },
+      {
+        name: "Postman",
+        link: "https://www.getpostman.com/",
+        tooltip: "Use Postman to test APIs!",
       },
       {
         name: "Ngrok",
         link: "https://ngrok.io",
         tooltip:
-          "Instantly create a public HTTPS url for a website running locally on your development machine using Ngrok"
+          "Instantly create a public URL for a website running locally on your development machine.",
       },
-      {
-        name: "Postman",
-        link: "https://www.getpostman.com/",
-        tooltip:
-          "A great tool for testing RESTful APIs made by others or yourself"
-      }
-    ]
-  }
+    ],
+  },
 ];
 
 export { starterPackData };
