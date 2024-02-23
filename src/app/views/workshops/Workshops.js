502bebe417fb4009f75a93b487605a146578dace
Alan Chang <alan@alanchang.xyz>
2024-02-22 20:06:15 -0800

    Prettier works with separate file extensions now

--- src/app/views/workshops/Workshops.js
+++ src/app/views/workshops/Workshops.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Workshops.scss";
 
-import { WorkshopCard, Footer } from "app/components"
+import { WorkshopCard, Footer } from "app/components";
 import { workshopsData } from "assets/data/workshops-info.js";
 
-function Workshops() {
-    return (
-        <div className="workshops">
-            <h2>Workshops</h2>
-            <h5>For time and location info, check the <Link to='/schedule'>Schedule</Link> page!</h5>
-            {workshopsData.map(workshop =>
-                WorkshopCard(workshop)
-            )}
-            <Footer />
-        </div>
-    );
-}
+const Workshops = () => {
+  return (
+    <div className="workshops">
+      <h2>Workshops</h2>
+      <h5>
+        For time and location info, check the{" "}
+        <Link to="/schedule">Schedule</Link> page!
+      </h5>
+      {workshopsData.map((workshop) => WorkshopCard(workshop))}
+      <Footer />
+    </div>
+  );
+};
 
 export default Workshops;
