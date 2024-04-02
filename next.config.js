670db93a05a932840f9a466e0deb4df40459d61f
Albert Wang <alberw5@uci.edu>
2024-04-02 16:03:51 -0700

    Add 2024 VH Applications (#257)
    * add hacker and volunteer applications
    
    * add mentor application

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
+	// output: "export",
 	reactStrictMode: true,
-  // rewrites: async () => ([]),
-  // redirects: async () => ([]),
+	// rewrites: async () => ([]),
+	redirects: async () => [
+		{
+			source: "/apply",
+			destination: "https://tally.so/r/wvyaW0",
+			permanent: true,
+		},
+		{
+			source: "/mentor",
+			destination: "https://x03p1nm9c8s.typeform.com/to/tbReYJHH",
+			permanent: true,
+		},
+		{
+			source: "/volunteer",
+			destination: "https://tally.so/r/w48aRA",
+			permanent: true,
+		},
+	],
 };
 
 module.exports = nextConfig;
--- src/app/(main)/(home)/page.jsx
+++ src/app/(main)/(home)/page.jsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
 
 import { comfortaa } from "@/app/fonts";
+
+import { VenusButton } from "@/app/components";
+
 import vhTitle from "@/assets/images/titles/vh-title-launch.svg";
 import "./Home.scss";
 
@@ -27,12 +32,18 @@ const Home = () => {
 						alt="VenusHacks Title Logo"
 						id="venushacks-title"
 					/>
-					<h4 className={comfortaa.variable} id="date">{VH_DATE}</h4>
+					<h4 className={comfortaa.variable} id="date">
+						{VH_DATE}
+					</h4>
 					<p id="tagline">UC Irvine&apos;s largest women-centric hackathon</p>
+					<p id="apps">Apps are now open!</p>
+					<VenusButton text="Hacker" url="/apply" />
+					<VenusButton text="Mentor" url="/mentor" />
+					<VenusButton text="Volunteer" url="/volunteer" />
 				</div>
 			</section>
 		</div>
-	)
+	);
 };
 
 export default Home;
