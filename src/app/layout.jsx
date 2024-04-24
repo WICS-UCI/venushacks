687ccfddac9edfb1f99374cb0113089ad8bdf2c3
Albert Wang <alberw5@uci.edu>
2024-04-24 01:56:09 -0700

    add bootstraps styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,31 +1,32 @@
 import { varela } from "./fonts";
+import "@/app/lib/styles/bootstrap.scss";
 
 export const metadata = {
-  title: {
-    default: "VenusHacks",
-    template: "%s | VenusHacks",
-  },
-  description: "UC Irvine's women-centric hackathon",
-  openGraph: {
-    title: "VenusHacks",
-    description: "UC Irvine's women-centric hackathon",
-    url: "https://venushacks.com",
-    siteName: "VenusHacks",
-    locale: "en_US",
-    type: "website",
-  },
+	title: {
+		default: "VenusHacks",
+		template: "%s | VenusHacks",
+	},
+	description: "UC Irvine's women-centric hackathon",
+	openGraph: {
+		title: "VenusHacks",
+		description: "UC Irvine's women-centric hackathon",
+		url: "https://venushacks.com",
+		siteName: "VenusHacks",
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export const viewport = {
-  themeColor: "#000000",
-  width: "device-width",
-  initialScale: 1,
+	themeColor: "#000000",
+	width: "device-width",
+	initialScale: 1,
 };
 
 const RootLayout = ({ children }) => (
-  <html lang="en">
-    <body className={varela.className}>{children}</body>
-  </html>
+	<html lang="en">
+		<body className={varela.className}>{children}</body>
+	</html>
 );
 
 export default RootLayout;
