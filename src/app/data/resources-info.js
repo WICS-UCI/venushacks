17c9a43e19e6737ccfa91aa1f5540e8e23b8abf8
Rylie Kim <minjeok6@uci.edu>
2024-05-21 15:34:29 -0700

    Committing changes for resources-page

--- src/app/data/resources-info.js
+++ src/app/data/resources-info.js
@@ -7,28 +7,33 @@ let starterPackData = [
         name: "Frontend Starter Code",
         link: "https://github.com/HackAtUCI/zothacks-frontend-startercode",
         tooltip: "A basic webpage built using HTML, CSS, and JavaScript.",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "React Workshop",
         link: "https://github.com/nmpham2/hack-at-uci-react-intro",
         tooltip:
           "Learn how to use React, a JavaScript library for building user interfaces.",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "React Native",
         link: "https://reactnative.dev/",
         tooltip: "React, but for mobile!",
+        image: "/assets/images/rocketship.png",
       },
       {
         name: "Android Studio",
         link: "https://developer.android.com/studio",
         tooltip:
           "Need a mobile emulator? Then Android Studio is the tool for you!",
+        image: "/assets/images/laptop.png",
       },
       {
         name: "Flutter SDK",
         link: "https://docs.flutter.dev/",
         tooltip: "Tackle mobile development with this modern mobile SDK!",
+        image: "/assets/images/planet-3.png",
       },
     ],
   },
@@ -40,27 +45,32 @@ let starterPackData = [
         name: "Simple Flask Example",
         link: "https://github.com/HackAtUCI/zothacks-backend-startercode",
         tooltip: "A basic backend example using Flask.",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "Webscraping Workshop",
         link: "https://github.com/will-hou/hackatuci-learn-webscraping-2023",
         tooltip: "Learn how to extract data from websites!",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "Free Public APIs",
         link: "https://github.com/public-apis/public-apis",
         tooltip: "A collective list of free APIs to use.",
+        image: "/assets/images/rocketship.png",
       },
       {
         name: "FastAPI",
         link: "https://fastapi.tiangolo.com/",
         tooltip: "A popular API Framework for Python.",
+        image: "/assets/images/laptop.png",
       },
       {
         name: "PeterPortal Public API",
         link: "https://api.peterportal.org/",
         tooltip:
           "PeterPortal API provides data about UCI professors, courses, and grades.",
+        image: "/assets/images/planet-3.png",
       },
     ],
   },
@@ -72,21 +82,25 @@ let starterPackData = [
         name: "CockroachDB",
         link: "https://www.cockroachlabs.com/docs/",
         tooltip: "A distributed SQL database from our sponsor Cockroach Labs!",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "MongoDB",
         link: "https://www.mongodb.com/",
         tooltip: "A popular NoSQL database.",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "PostgreSQL",
         link: "https://www.postgresql.org/",
         tooltip: "A popular object-relational database.",
+        image: "/assets/images/rocketship.png",
       },
       {
         name: "MySQL",
         link: "https://www.mysql.com/",
         tooltip: "A popular relational database.",
+        image: "/assets/images/laptop.png",
       },
     ],
   },
@@ -99,21 +113,25 @@ let starterPackData = [
         name: "GitHub Pages",
         link: "https://pages.github.com/",
         tooltip: "Deploy your static site on GitHub Pages!",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "Vercel",
         link: "https://vercel.com/",
         tooltip: "Deploy your frontend app easily!",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "Google Cloud Platform",
         link: "https://cloud.google.com/docs",
         tooltip: "Scale your project with Google Cloud!",
+        image: "/assets/images/rocketship.png",
       },
       {
         name: "Amazon Web Services",
         link: "https://docs.aws.amazon.com",
         tooltip: "Deploy your app to this large cloud computing platform!",
+        image: "/assets/images/laptop.png",
       },
     ],
   },
@@ -126,18 +144,21 @@ let starterPackData = [
         link: "https://docs.unity3d.com/Manual/index.html",
         tooltip:
           "Official documentation for Unity, a cross-platform game engine",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "Godot",
         link: "https://godotengine.org/",
         tooltip:
           "Godot provides a huge set of common tools for game development",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "PyGame",
         link: "https://www.pygame.org/wiki/GettingStarted",
         tooltip:
           "Official documentation for PyGame, a cross-platform set of Python modules designed for writing video games",
+        image: "/assets/images/rocketship.png",
       },
     ],
   },
@@ -151,16 +172,19 @@ let starterPackData = [
         link: "https://www.tensorflow.org/",
         tooltip:
           "A comprehensive library for machine learning and artificial intelligence.",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "Pytorch",
         link: "https://pytorch.org/",
         tooltip: "A machine learning framework.",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "Scikit-learn",
         link: "https://scikit-learn.org/stable/",
         tooltip: "A data analysis and machine learning library.",
+        image: "/assets/images/rocketship.png",
       },
     ],
   },
@@ -172,23 +196,27 @@ let starterPackData = [
         name: "Git",
         link: "https://docs.google.com/presentation/d/1Omd_azmTLQqrIjc6Bkva7PetFeQosLpfyLox8xDA5Mg/edit",
         tooltip: "A popular version control tool!",
+        image: "/assets/images/telescope-image.png",
       },
       {
         name: "VS Code",
         link: "https://code.visualstudio.com/",
         tooltip:
           "A streamlined code editor with support for debugging, task running, and version control.",
+        image: "/assets/images/ufo-1.png",
       },
       {
         name: "Postman",
         link: "https://www.getpostman.com/",
         tooltip: "Use Postman to test APIs!",
+        image: "/assets/images/rocketship.png",
       },
       {
         name: "Ngrok",
         link: "https://ngrok.io",
         tooltip:
           "Instantly create a public URL for a website running locally on your development machine.",
+        image: "/assets/images/laptop.png",
       },
     ],
   },
--- src/app/views/resources/Resources.jsx
+++ src/app/views/resources/Resources.jsx
@@ -1,45 +1,55 @@
+import React from "react";
 import { Tooltip, Footer } from "src/app/components";
 import { starterPackData } from "src/app/data/resources-info";
+import { Carousel, Stack, Card } from 'react-bootstrap';
 
 import "./Resources.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Resources() {
-	return (
-		<div className="starter-packs">
-			<h2>Resources</h2>
-			{starterPackData.map(function (starterPack) {
-				return (
-					<div className="starter-pack-card" key={starterPack.name}>
-						<div className="starter-pack-card-information">
-							<h3>{starterPack.name}</h3>
-							<p>{starterPack.description}</p>
-						</div>
-						<div className="starter-pack-card-links">
-							{starterPack.packs.map(function (pack) {
-								return (
-									<Tooltip content={pack.tooltip} key={pack.name}>
-										<a
-											className="starter-pack-card-link"
-											href={pack.link}
-											target={pack.link.startsWith("/") ? "_self" : "_blank"}
-											rel="noopener noreferrer"
-										>
-											<div>
-												<h4>{pack.name}</h4>
-											</div>
-										</a>
-									</Tooltip>
-								);
-							})}
-						</div>
-					</div>
-				);
-			})}
-			<Footer />
-		</div>
-	);
-}
+    return (
+        <div className="starter-packs">
+            <h2>Resources</h2>
+            {starterPackData.map((starterPack) => (
+                <div className="starter-pack-card" key={starterPack.name}>
+                    <div className="starter-pack-card-information">
+                        <h3>{starterPack.name}</h3>
+                        <p>{starterPack.description}</p>
 
-// const Resources = () => process.env.NEXT_PUBLIC_MAINTENANCE_MODE_RESOURCES ? redirect("/") : <p>Resources</p>;
+                        <div className="starter-pack-card-links">
+                            <Carousel style={{ height: 500 }}>
+                                {[...Array(Math.ceil(starterPack.packs.length / 3))].map((_, index) => (
+                                    <Carousel.Item key={index} className="carousel-item">
+                                        <Stack direction="horizontal" className="stack">
+                                            {starterPack.packs.slice(index * 3, index * 3 + 3).map((pack, packIndex) => (
+                                                <Card key={packIndex} className="card">
+                                                    <Tooltip className='tooltip_link' content={pack.tooltip} key={pack.name}>
+                                                        <a
+                                                            className="starter-pack-card-link"
+                                                            href={pack.link}
+                                                            target={pack.link.startsWith("/") ? "_self" : "_blank"}
+                                                            rel="noopener noreferrer"
+                                                        >
+                                                            <h4>{pack.name}</h4>
+                                                            <img
+                                                                src={pack.image}
+                                                                alt={pack.name}
+                                                            />
+                                                        </a>
+                                                    </Tooltip>
+                                                </Card>
+                                            ))}
+                                        </Stack>
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <Footer />
+        </div>
+    );
+}
 
 export default Resources;
