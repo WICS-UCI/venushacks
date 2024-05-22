f9d499af11a769dff77a8654659079a19256364a
Albert Wang <alberw5@uci.edu>
2024-05-21 19:16:29 -0700

    Add sponsors

--- src/app/views/home/Home.jsx
+++ src/app/views/home/Home.jsx
@@ -1,15 +1,22 @@
 import FAQs from "src/app/components/faqs/FAQs";
-import { VenusButton } from "src/app/components";
+import Sponsor from "src/app/components/sponsor/Sponsor";
 
 import vhTitle from "/assets/images/titles/vh-title-launch.svg";
 import about_title from "/assets/images/titles/vh-about.svg";
 import faq_title from "/assets/images/titles/vh-faq.svg";
+import sponsors_title from "/assets/images/titles/vh-sponsors.svg";
+
+import odit from "/assets/images/current-sponsors/odit.png";
+import melissa from "/assets/images/current-sponsors/melissa.png";
+import antrepreneur from "/assets/images/current-sponsors/antrepreneur-center.png";
+import oai from "/assets/images/current-sponsors/office-access-inclusion.png";
+import spfb from "/assets/images/current-sponsors/spfb.png";
 
 import "./Home.scss";
 
 const VH_DATE = "May 24 - 26, 2024";
 const CONTACT_EMAIL = "venushacks.uci@gmail.com";
-// const CORPORATE_EMAIL = 'sponsorships@venushacks.com';
+const CORPORATE_EMAIL = "sponsorships@venushacks.com";
 
 const Home = () => {
 	return (
@@ -75,11 +82,52 @@ const Home = () => {
 			</section>
 			<section id="faq">
 				<img className="section-title" src={faq_title} alt="FAQ" />
-				<FAQs />
+				<div id="faq-container">
+					<FAQs />
+				</div>
 				<p id="faq-contact-us">
 					Additional logistic questions? Contact us at&nbsp;
 					<a href={`mailto:${CONTACT_EMAIL}`} target="_top">
 						{CONTACT_EMAIL}
+					</a>
+					.
+				</p>
+			</section>
+			<section id="sponsors">
+				<img className="section-title" src={sponsors_title} alt="Sponsor" />
+				<div id="sponsors-container">
+					<div className="logo-wrapper largest">
+						<Sponsor imgId="odit" imgSrc={odit} url="https://odit.uci.edu/" />
+					</div>
+					<div className="logo-wrapper large">
+						<Sponsor
+							imgId="melissa"
+							imgSrc={melissa}
+							url="https://www.melissa.com/"
+						/>
+					</div>
+					<div className="logo-wrapper medium">
+						<Sponsor
+							imgId="antrepreneur"
+							imgSrc={antrepreneur}
+							url="https://antrepreneur.uci.edu/"
+						/>
+					</div>
+					<div className="logo-wrapper medium">
+						<Sponsor imgId="oai" imgSrc={oai} url="https://oai.tech.uci.edu/" />
+					</div>
+					<div className="logo-wrapper medium">
+						<Sponsor
+							imgId="spfb"
+							imgSrc={spfb}
+							url="https://asuci.uci.edu/president/spfb/"
+						/>
+					</div>
+				</div>
+				<p className="sponsor-contact-us">
+					Join our movement, contact us at&nbsp;
+					<a href={`mailto:${CORPORATE_EMAIL}`} target="_top">
+						{CORPORATE_EMAIL}
 					</a>
 					.
 				</p>
