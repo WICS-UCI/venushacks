9393b161601c654553fda05d7a3d1bb2cf6b74a3
Albert Wang <alberw5@uci.edu>
2024-05-24 12:10:30 -0700

    update workshops header

--- src/app/views/workshops/Workshops.jsx
+++ src/app/views/workshops/Workshops.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import { WorkshopCard, Footer } from "src/app/components";
 import { workshopsData } from "src/app/data/workshops-info.js";
+import workshopsTitle from "/assets/images/titles/vh-workshops-title.png";
 
 import "./Workshops.scss";
 
@@ -9,7 +10,7 @@ const Workshops = () => {
 	return (
 		<div className="workshops">
 			<div id="workshops_header">
-				<h2>Workshops</h2>
+				<img src={workshopsTitle} alt="Workshops" id="title-img" />
 				<h5>
 					For time and location info, check the{" "}
 					<Link to="/schedule">Schedule</Link> page!
