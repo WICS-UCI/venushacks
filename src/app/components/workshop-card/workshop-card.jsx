fe01afa9b218a6741f4c9ce7cb07713a709f1227
Albert Wang <alberw5@uci.edu>
2024-05-24 11:48:21 -0700

    update workshops page for 2024

--- src/app/components/workshop-card/workshop-card.jsx
+++ src/app/components/workshop-card/workshop-card.jsx
@@ -6,9 +6,11 @@ const renderButtons = (buttons) => {
 	return buttons.map((button) => {
 		const { link, description } = button;
 		return link ? (
-			<a href={link} target="_blank" rel="noopener noreferrer">
-				{description}
-			</a>
+			<div className="prereq-button">
+				<a href={link} target="_blank" rel="noopener noreferrer">
+					{description}
+				</a>
+			</div>
 		) : (
 			<p>{description}</p>
 		);
@@ -20,33 +22,28 @@ function WorkshopCard(workshop) {
 
 	return (
 		<div className="workshop-card">
-			<div className="workshop-card-header">
-				<div>
-					<h4>{title}</h4>
-					<h5>
-						Hosted by{" "}
-						<a href={host.link} target="_blank" rel="noopener noreferrer">
-							{host.name}
-						</a>
-					</h5>
-				</div>
-				<div className="right pink-buttons">
-					{recap &&
-						renderButtons(
-							Object.entries(recap).map((pair) => {
-								const [key, val] = pair;
-								return { description: key, link: val };
-							})
-						)}
-				</div>
+			<div className="workshop-card-left">
+				<p className="workshop-card-title">{title}</p>
+				<p id="hosted-by">Hosted by</p>
+				<p id="host-name">{host.name}</p>
+				{/* recap??? */}
+				{recap &&
+					renderButtons(
+						Object.entries(recap).map((pair) => {
+							const [key, val] = pair;
+							return { description: key, link: val };
+						})
+					)}
+				{prereqs && (
+					<div className="workshop-card-prereqs">
+						<h5 id="prerequisites-title">Prequisites:</h5>
+						{renderButtons(prereqs)}
+					</div>
+				)}
 			</div>
-			{description}
-			{prereqs && (
-				<div className="workshop-card-prereqs pink-buttons">
-					<h5>Prequisites:</h5>
-					{renderButtons(prereqs)}
-				</div>
-			)}
+			<div className="workshop-card-right">
+				<p className="workshop-card-description">{description}</p>
+			</div>
 		</div>
 	);
 }
--- src/app/views/workshops/Workshops.jsx
+++ src/app/views/workshops/Workshops.jsx
@@ -1,24 +1,25 @@
 import { Link } from "react-router-dom";
 
+import { WorkshopCard, Footer } from "src/app/components";
+import { workshopsData } from "src/app/data/workshops-info.js";
+
 import "./Workshops.scss";
 
-// import { WorkshopCard, Footer } from "./app/components";
-import { WorkshopCard, Footer } from "../../components";
-// import { workshopsData } from "./assets/data/workshops-info.js";
-import { workshopsData } from "../../data/workshops-info.js";
-
-function Workshops() {
+const Workshops = () => {
 	return (
 		<div className="workshops">
-			<h2>Workshops</h2>
-			<h5>
-				For time and location info, check the{" "}
-				<Link to="/schedule">Schedule</Link> page!
-			</h5>
+			<div id="workshops_header">
+				<h2>Workshops</h2>
+				<h5>
+					For time and location info, check the{" "}
+					<Link to="/schedule">Schedule</Link> page!
+				</h5>
+			</div>
+
 			{workshopsData.map((workshop) => WorkshopCard(workshop))}
 			<Footer />
 		</div>
 	);
-}
+};
 
 export default Workshops;
