a1feee6b6f1ebf8d741924f13e329d605f602f7f
MehekB <68450274+MehekB@users.noreply.github.com>
2024-05-24 11:13:44 -0700

    2024 Resources page (#265)
    * Add images for the starter pack
    
    * Committing changes for resources-page
    
    * uninstalled bootstrap, removed imported styling
    
    * added resources title, fixed tooltip hover
    
    * Update carousels to display one resource at a time on smaller screens
    
    * Update pnpm-lock.yaml
    
    * remove extra space below footer
    
    ---------
    
    Co-authored-by: Rylie Kim <minjeok6@uci.edu>
    Co-authored-by: Albert Wang <alberw5@uci.edu>

--- src/app/views/resources/Resources.jsx
+++ src/app/views/resources/Resources.jsx
@@ -1,45 +1,205 @@
+import { useState, useEffect } from "react";
 import { Tooltip, Footer } from "src/app/components";
 import { starterPackData } from "src/app/data/resources-info";
+import { Carousel, Stack, Card } from "react-bootstrap";
+
+import next_icon from "/assets/images/next-icon.svg";
+import prev_icon from "/assets/images/prev-icon.svg";
 
 import "./Resources.scss";
 
+function getWindowDimensions() {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height,
+	};
+}
+
 function Resources() {
+	const [uiIndex, setUIIndex] = useState(0);
+	const [backendIndex, setBackendIndex] = useState(0);
+	const [databasesIndex, setDatabasesIndex] = useState(0);
+	const [deploymentIndex, setDeploymentIndex] = useState(0);
+	const [gamingIndex, setGamingIndex] = useState(0);
+	const [aiIndex, setAIIndex] = useState(0);
+	const [generalIndex, setGeneralIndex] = useState(0);
+
+	const [windowDimensions, setWindowDimensions] = useState(
+		getWindowDimensions()
+	);
+
+	const handleUISelect = (selectedIndex) => {
+		setUIIndex(selectedIndex);
+	};
+
+	const handleBackendSelect = (selectedIndex) => {
+		setBackendIndex(selectedIndex);
+	};
+
+	const handleDatabasesSelect = (selectedIndex) => {
+		setDatabasesIndex(selectedIndex);
+	};
+
+	const handleDeploymentSelect = (selectedIndex) => {
+		setDeploymentIndex(selectedIndex);
+	};
+
+	const handleGamingSelect = (selectedIndex) => {
+		setGamingIndex(selectedIndex);
+	};
+
+	const handleAISelect = (selectedIndex) => {
+		setAIIndex(selectedIndex);
+	};
+
+	const handleGeneralIndex = (selectedIndex) => {
+		setGeneralIndex(selectedIndex);
+	};
+
+	const indexes = [
+		uiIndex,
+		backendIndex,
+		databasesIndex,
+		deploymentIndex,
+		gamingIndex,
+		aiIndex,
+		generalIndex,
+	];
+
+	const handleIndexes = [
+		setUIIndex,
+		setBackendIndex,
+		setDatabasesIndex,
+		setDeploymentIndex,
+		setGamingIndex,
+		setAIIndex,
+		setGeneralIndex,
+	];
+
+	useEffect(() => {
+		function handleResize() {
+			const dimensions = getWindowDimensions();
+			if (dimensions.width <= 900) {
+				handleIndexes.map((handleIndex) => {
+					handleIndex(0);
+				});
+			} else if (dimensions.width > 900) {
+				handleIndexes.map((handleIndex) => {
+					handleIndex(0);
+				});
+			}
+			setWindowDimensions(dimensions);
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
 	return (
 		<div className="starter-packs">
-			<h2>Resources</h2>
-			{starterPackData.map(function (starterPack) {
-				return (
-					<div className="starter-pack-card" key={starterPack.name}>
-						<div className="starter-pack-card-information">
-							<h3>{starterPack.name}</h3>
-							<p>{starterPack.description}</p>
-						</div>
+			<h2>
+				<img className="title-img" src="/assets/images/resources_title.png" />
+			</h2>
+			{starterPackData.map((starterPack, index) => (
+				<div className="starter-pack-card" key={starterPack.name}>
+					<div className="starter-pack-card-information">
+						<h3>{starterPack.name}</h3>
+						<p>{starterPack.description}</p>
+
 						<div className="starter-pack-card-links">
-							{starterPack.packs.map(function (pack) {
-								return (
-									<Tooltip content={pack.tooltip} key={pack.name}>
-										<a
-											className="starter-pack-card-link"
-											href={pack.link}
-											target={pack.link.startsWith("/") ? "_self" : "_blank"}
-											rel="noopener noreferrer"
-										>
-											<div>
-												<h4>{pack.name}</h4>
-											</div>
-										</a>
-									</Tooltip>
-								);
-							})}
+							{windowDimensions.width <= 900 ? (
+								<Carousel
+									activeIndex={indexes[index]}
+									onSelect={handleIndexes[index]}
+									nextIcon={<img src={next_icon} alt="Next" />}
+									prevIcon={<img src={prev_icon} alt="Previous" />}
+									style={{ height: 500 }}
+								>
+									{[...Array(Math.ceil(starterPack.packs.length))].map(
+										(_, index) => (
+											<Carousel.Item key={index} className="carousel-item">
+												<Stack direction="horizontal" className="stack">
+													{starterPack.packs
+														.slice(index, index + 1)
+														.map((pack, packIndex) => (
+															<Card key={packIndex} className="card">
+																<Tooltip
+																	className="tooltip_link"
+																	content={pack.tooltip}
+																	key={pack.name}
+																>
+																	<a
+																		className="starter-pack-card-link"
+																		href={pack.link}
+																		target={
+																			pack.link.startsWith("/")
+																				? "_self"
+																				: "_blank"
+																		}
+																		rel="noopener noreferrer"
+																	>
+																		<h4>{pack.name}</h4>
+																		<img src={pack.image} alt={pack.name} />
+																	</a>
+																</Tooltip>
+															</Card>
+														))}
+												</Stack>
+											</Carousel.Item>
+										)
+									)}
+								</Carousel>
+							) : (
+								<Carousel
+									activeIndex={indexes[index]}
+									onSelect={handleIndexes[index]}
+									nextIcon={<img src={next_icon} alt="Next" />}
+									prevIcon={<img src={prev_icon} alt="Previous" />}
+									style={{ height: 500 }}
+								>
+									{[...Array(Math.ceil(starterPack.packs.length / 3))].map(
+										(_, index) => (
+											<Carousel.Item key={index} className="carousel-item">
+												<Stack direction="horizontal" className="stack">
+													{starterPack.packs
+														.slice(index * 3, index * 3 + 3)
+														.map((pack, packIndex) => (
+															<Card key={packIndex} className="card">
+																<Tooltip
+																	className="tooltip_link"
+																	content={pack.tooltip}
+																	key={pack.name}
+																>
+																	<a
+																		className="starter-pack-card-link"
+																		href={pack.link}
+																		target={
+																			pack.link.startsWith("/")
+																				? "_self"
+																				: "_blank"
+																		}
+																		rel="noopener noreferrer"
+																	>
+																		<h4>{pack.name}</h4>
+																		<img src={pack.image} alt={pack.name} />
+																	</a>
+																</Tooltip>
+															</Card>
+														))}
+												</Stack>
+											</Carousel.Item>
+										)
+									)}
+								</Carousel>
+							)}
 						</div>
 					</div>
-				);
-			})}
+				</div>
+			))}
 			<Footer />
 		</div>
 	);
 }
-
-// const Resources = () => process.env.NEXT_PUBLIC_MAINTENANCE_MODE_RESOURCES ? redirect("/") : <p>Resources</p>;
 
 export default Resources;
