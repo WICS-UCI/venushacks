18277da5a4a9f3a30b690f9f48407f084c53bee0
Albert Wang <alberw5@uci.edu>
2024-05-24 20:59:53 -0700

    change schedule timer to hacking ends

--- src/app/views/schedule/Schedule.jsx
+++ src/app/views/schedule/Schedule.jsx
@@ -8,24 +8,27 @@ import {
 import "./Schedule.scss";
 import { Container } from "react-bootstrap";
 
-const HACKING_DEADLINE = "24 May 2024 21:00:00 PDT";
-// const HACKING_DEADLINE = "26 May 2024 09:00:00 PDT";
+// const HACKING_DEADLINE = "24 May 2024 21:00:00 PDT";
+const HACKING_DEADLINE = "26 May 2024 09:00:00 PDT";
 
 const Schedule = () => (
 	<div className="Schedule">
 		<section id="schedule-block">
 			<div className="schedule-header">
-				<img src="../../../assets/images/schedule/schedule-header.png" alt="Schedule Header"></img>
+				<img
+					src="../../../assets/images/schedule/schedule-header.png"
+					alt="Schedule Header"
+				></img>
 			</div>
 			<h4 className="schedule-hacking-ends-in">
-				All times in PDT. Hacking begins in:
+				All times in PDT. Hacking ends in:
 			</h4>
 			<div className="schedule-countdown">
 				<Container className="container-style">
 					<Countdown date={HACKING_DEADLINE} />
 				</Container>
 			</div>
-			<h4 className="schedule-hacking-ends-in">See you soon!</h4>
+			<h4 className="schedule-hacking-ends-in">Blast off!</h4>
 			<ScheduleCard />
 		</section>
 		<Footer />
