91b45f52297bb003421838744bbab7d7a72250fa
Leo <leonardoy.siu@gmail.com>
2025-02-11 18:30:57 -0800

    convert images to svg, add title and subtitle with animation

--- src/app/views/coming-soon/ComingSoon.jsx
+++ src/app/views/coming-soon/ComingSoon.jsx
@@ -3,9 +3,45 @@ import "./ComingSoon.scss";
 const ComingSoon = () => {
 	return (
 		<div className="ComingSoon">
-            <div id="left-fishes" />
-            <div id="right-fishes" />
-            <div id="coral" />
+			<div id="left-fishes" />
+			<div id="right-fishes" />
+			<div id="coral" />
+
+			<div className="coming-soon-text">
+				<div id="coming-soon-title-container">
+					<h1 id="title-v" className="coming-soon-title title-animation">
+						V
+					</h1>
+					<h1 id="title-e" className="coming-soon-title">
+						E
+					</h1>
+					<h1 id="title-n" className="coming-soon-title">
+						N
+					</h1>
+					<h1 id="title-u" className="coming-soon-title">
+						U
+					</h1>
+					<h1 id="title-s1" className="coming-soon-title">
+						S
+					</h1>
+					<h1 id="title-h" className="coming-soon-title">
+						H
+					</h1>
+					<h1 id="title-a" className="coming-soon-title">
+						A
+					</h1>
+					<h1 id="title-c" className="coming-soon-title">
+						C
+					</h1>
+					<h1 id="title-k" className="coming-soon-title">
+						K
+					</h1>
+					<h1 id="title-s2" className="coming-soon-title title-animation">
+						S
+					</h1>
+				</div>
+				<div id="coming-soon-subtitle">Coming Soon in Spring 2025...</div>
+			</div>
 		</div>
 	);
 };
