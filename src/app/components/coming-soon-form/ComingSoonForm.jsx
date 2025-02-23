73422ab46423a8112f57c89b193fbd40ef2adaec
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2025-02-22 17:08:28 -0800

    [Coming Soon] Mailchimp Integration (#281)
    * Added back Navbar
    
    * Added mailchimp form
    
    * Form works and submits
    
    * Styled form for mobile views
    
    * Moved button to its own component
    
    * Removed unused label
    
    * improved mobile view
    
    * Updated fonts and backgrounds for form
    
    * Fixed coloring for focused button

---
+++ src/app/components/coming-soon-form/ComingSoonForm.jsx
@@ -0,0 +1,27 @@
+import PrimaryButton from "src/app/components/primary-button/PrimaryButton";
+
+import "./ComingSoonForm.scss";
+
+export default function ComingSoonForm() {
+	return (
+		<form
+			action="https://venushacks.us14.list-manage.com/subscribe/post"
+			method="POST"
+			className="email-form"
+		>
+			<input type="hidden" name="u" value="f6273e8cb99a5b38c895e6292" />
+			<input type="hidden" name="id" value="7afebb41eb" />
+
+			<input
+				type="email"
+				className="email-input"
+				autoCapitalize="off"
+				autoCorrect="off"
+				name="MERGE0"
+				id="MERGE0"
+				placeholder="Email Address"
+			/>
+			<PrimaryButton type="submit">Notify Me!</PrimaryButton>
+		</form>
+	);
+}
--- src/app/views/coming-soon/ComingSoon.jsx
+++ src/app/views/coming-soon/ComingSoon.jsx
@@ -1,14 +1,21 @@
+import Container from "react-bootstrap/Container";
+
 import ComingSoonTitle from "src/app/components/coming-soon/ComingSoonTitle";
+import ComingSoonForm from "src/app/components/coming-soon-form/ComingSoonForm";
+
 import "./ComingSoon.scss";
 
 const ComingSoon = () => {
 	return (
-		<div className="ComingSoon">
+		<div className="coming-soon-container">
 			<div id="left-fishes" />
 			<div id="right-fishes" />
 			<div id="coral" />
 
-			<ComingSoonTitle />
+			<Container className="d-flex flex-column align-items-center">
+				<ComingSoonTitle />
+				<ComingSoonForm />
+			</Container>
 		</div>
 	);
 };
