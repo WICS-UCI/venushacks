7196f9be3e1c021319ec7698c03f12ab351a0991
Leo <leonardoy.siu@gmail.com>
2025-03-23 22:18:11 -0700

    implement initial scroll animation

--- site/src/app/components/hero/Hero.jsx
+++ site/src/app/components/hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Title from "../title/Title";
 import PrimaryButton from "../primary-button/PrimaryButton";
@@ -6,9 +7,33 @@ import "./Hero.scss";
 
 
 const Hero = () => {
+	const [scrollY, setScrollY] = useState(0);
+
+	useEffect(() => {
+		const handleScroll = () => setScrollY(window.scrollY);
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
-		<div className="hero-container">
-			<div id="coral" />
+		<section className="hero-container">
+			{/* PARALLAX CORAL LAYERS */}
+			<div
+				id="coral1"
+				style={{ transform: `translateY(${scrollY * 0.2}px)` }}
+			/>
+			<div
+				id="coral2"
+				style={{ transform: `translateY(${scrollY * 0.4}px)` }}
+			/>
+			<div
+				id="coral3"
+				style={{ transform: `translateY(${scrollY * 0.6}px)` }}
+			/>
+			<div
+				id="coral4"
+				style={{ transform: `translateY(${scrollY * 0.8}px)` }}
+			/>
 
 			<Container className="d-flex flex-column align-items-center">
 				<Title />
@@ -17,7 +42,7 @@ const Hero = () => {
 					<PrimaryButton href="/sponsor" type="button">Sponsor Us</PrimaryButton>
 				</div>
 			</Container>
-		</div>
+		</section>
 	);
 };
 
