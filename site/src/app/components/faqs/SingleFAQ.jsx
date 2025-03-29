05a4afae7f48bb5e8c3f0a19a1a904b8981ed1cd
Chelsea Chen <sflake1225@gmail.com>
2025-03-28 17:53:35 -0700

    FAQ Section (#287)
    * Updated FAQ section title and body font, title text alignment, and the faq container width.
    
    * Created faq schema. Using block type for answers just in case we want to use markdown in the future.
    
    * Created FAQ fetch and use functions. Modified FAQ accordions from 2 column grid to 1 column on medium~larger screens.
    
    * successfully displayed faqs fetched from sanity to page.
    
    * Placed the toggle arrow on the left side of the accordion header. Removed contact us text at the bottom of section. Adjusted font sizes and width for mobile devices.
    
    * Uncommented all other sections.
    
    * Removed old section-title image.
    
    * Fixed a small mistake in SingleFAQ.
    
    * Removed console.log line from useQuestions.js

--- site/src/app/components/faqs/SingleFAQ.jsx
+++ site/src/app/components/faqs/SingleFAQ.jsx
@@ -1,9 +1,13 @@
+import { PortableText } from "@portabletext/react";
 import { Accordion } from "react-bootstrap";
 
 const SingleFAQ = ({ index, data }) => (
 	<Accordion.Item eventKey={index}>
 		<Accordion.Header>{data.question}</Accordion.Header>
-		<Accordion.Body>{data.answer}</Accordion.Body>
+		<Accordion.Body>
+			{/* Render the answer which is in PortableText format */}
+			<PortableText value={data.answer} />
+		</Accordion.Body>
 	</Accordion.Item>
 );
 
--- site/src/app/views/home/Home.jsx
+++ site/src/app/views/home/Home.jsx
@@ -62,17 +62,10 @@ const Home = () => {
 
 			{/* FAQ ************************/}
 			<section id="faq">
-				<img className="section-title" src={faq_title} alt="FAQ" />
 				<div id="faq-container">
+					<h2 className="section-title">FAQs</h2>
 					<FAQs />
 				</div>
-				<p id="faq-contact-us">
-					Additional logistic questions? Contact us at&nbsp;
-					<a href={`mailto:${CONTACT_EMAIL}`} target="_top">
-						{CONTACT_EMAIL}
-					</a>
-					.
-				</p>
 			</section>
 			<Sponsors />
 			<Partners />
