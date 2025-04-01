5fd03510597b4ce3d49e37b3e1a61485b595ca00
CLiang531 <73400286+CLiang531@users.noreply.github.com>
2025-04-01 14:42:32 -0700

    Footer (#288)
    * added icons in assets, created and styled footer
    
    * added footer styling for mobile view
    
    * moved Footer from Home.jsx to App.jsx
    
    * align footer text to center
    
    * converted font-size from px to rem; ran prettier
    
    ---------
    
    Co-authored-by: Cindy <liangc30@uci.edu>

--- site/src/app/components/footer/Footer.jsx
+++ site/src/app/components/footer/Footer.jsx
@@ -1,26 +1,60 @@
 import React from "react";
+
+import hackIcon from "/assets/images/hackatucilogo_icon.png";
+import wicsIcon from "/assets/images/wicslogo_icon.png";
+import heartIcon from "/assets/images/heart.svg";
+import emailIcon from "/assets/images/email_icon.svg";
+
 import "./Footer.scss";
 
 function Footer() {
 	return (
 		<footer id="footer">
-			<a href="mailto:venushacks.uci@gmail.com" target="_top">
-				<i className="fa fa-envelope"></i>
-			</a>
-			<a
-				href="https://www.tiktok.com/@venushacksuci?_t=8mcE7XPT13b&_r=1"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<i className="fa-brands fa-tiktok"></i>
-			</a>
-			<a
-				href="https://www.instagram.com/venushacksuci"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<i className="fa-brands fa-instagram"></i>
-			</a>
+			<div className="msg">
+				Made with <img src={heartIcon} id="love" alt="love"></img> by VenusHacks
+				Organizers
+			</div>
+			<div className="logos">
+				<a
+					href="https://hack.ics.uci.edu/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={hackIcon} alt="Hack at UCI Logo"></img>
+				</a>
+				<a
+					href="https://wics.ics.uci.edu/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={wicsIcon} alt="WICS Logo"></img>
+				</a>
+				<div className="divider"></div>
+				<a
+					href="https://www.instagram.com/venushacksuci"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i className="fa-brands fa-instagram"></i>
+				</a>
+				<a
+					href="https://www.tiktok.com/@venushacksuci"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i className="fa-brands fa-tiktok"></i>
+				</a>
+				<a
+					href="https://www.linkedin.com/company/venushacks/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i className="fa-brands fa-linkedin"></i>
+				</a>
+				<a href="mailto:venushacks.uci@gmail.com" target="_top">
+					<img id="color-change" src={emailIcon} alt="email"></img>
+				</a>
+			</div>
 		</footer>
 	);
 }
