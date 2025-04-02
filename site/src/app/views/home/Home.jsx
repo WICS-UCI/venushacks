7de4002ebfed49d7ac2ed5fc7ece17a775187329
Leo <leonardoy.siu@gmail.com>
2025-04-01 21:36:04 -0700

    implement bottom graphics section

--- site/src/app/views/home/Home.jsx
+++ site/src/app/views/home/Home.jsx
@@ -13,6 +13,7 @@ import Sponsors from "src/app/components/sponsors/Sponsors";
 // import meet_team_title from "/assets/images/titles/vh-meet-the-team.svg";
 
 import "./Home.scss";
+import BottomGraphic from "src/app/components/bottom-graphic/BottomGraphic";
 
 const VH_DATE = "May 24 - 26, 2024";
 const CONTACT_EMAIL = "venushacks.uci@gmail.com";
@@ -21,39 +22,6 @@ const Home = () => {
 	return (
 		<div className="Home">
 			{/* HERO **********************/}
-			<section id="hero">
-				<div id="hero-right">
-					<img
-						src={vh_title}
-						alt="VenusHacks Title Logo"
-						id="venushacks-title"
-					/>
-					<h4 id="date">{VH_DATE}</h4>
-					<p id="tagline">UC Irvine&apos;s largest women-centric hackathon</p>
-					<VenusButton url="/feedback" text="Feedback Form" />
-					<VenusButton
-						url="https://docs.google.com/presentation/d/1J8zlxYDR8AaiQXu7p_qg8xuav4qieKvU9AOkuwOJIhk/edit?usp=sharing"
-						text="Closing Ceremony"
-					/>
-				</div>
-				<div id="astronaut-animation">
-					{/* These assets are a background of a div instead of imgs to prevent
-					them from being able to be saved to camera roll on iOS */}
-					<div id="boba" />
-					<div id="astronaut" />
-					<div id="laptop" />
-					<div id="shiba-inu" />
-				</div>
-			</section>
-			<div id="planet" />
-			<h2 id="tanesha-moody-title">Tanesha Moody's Closing Speech</h2>
-			<iframe
-				src="https://drive.google.com/file/d/18y1D-co2T84_f287Eqj7NunVVTRM1-AP/preview"
-				width="640"
-				height="480"
-				allow="autoplay"
-				id="tanesha-moody"
-			></iframe>
 
 			{/* ABOUT **********************/}
 			<section id="about">
@@ -67,22 +35,19 @@ const Home = () => {
 					<FAQs />
 				</div>
 			</section>
-			<Sponsors />
-			<Partners />
+			{/* <Sponsors />
+			<Partners /> */}
 
 			{/* SPEAKERS ******************/}
 			<section id="speakers">
-				<img className="section-title" src={speakers_title} alt="Speakers" />
-				<Speakers />
 			</section>
 
 			<section id="meet-team">
-				<img
-					className="section-title"
-					src={meet_team_title}
-					alt="Meet the Team"
-				/>
-				<Team />
+			</section>
+
+			{/* BOTTOM GRAPHIC ******************/}
+			<section id="bottom-graphic">
+				<BottomGraphic />
 			</section>
 
 			<Footer />
