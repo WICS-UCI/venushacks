bc1d4954657cc6f8e2b024063a2c344d4c4fec87
ianbb <ianbbdai@gmail.com>
2025-04-01 21:31:05 -0700

    Refactored CoralParallaxSection

--- site/src/app/components/coral-parallax-section/CoralParallaxSection.jsx
+++ site/src/app/components/coral-parallax-section/CoralParallaxSection.jsx
@@ -1,170 +1,48 @@
-import { useRef, useState, useEffect } from "react";
 import { Parallax } from "react-scroll-parallax";
 import Container from "react-bootstrap/Container";
 import Title from "../title/Title";
 import PrimaryButton from "../primary-button/PrimaryButton";
+
 import "./CoralParallaxSection.scss";
 
-const coralImages = [
-	"/assets/images/coral4.svg",
-	"/assets/images/coral3.svg",
-	"/assets/images/coral2.svg",
-	"/assets/images/coral1.svg",
+const translatePositions = [
+	[0, 35],
+	[0, 65],
+	[0, 110],
 ];
 
-const parallelCorals = coralImages.slice(0, coralImages.length - 1);
-const darkBlueCoral = coralImages[coralImages.length - 1];
-
-const SCROLL_INCREMENT = -20;
-const SVG_ASPECT_RATIO = 1728 / 1117;
-const PARALLAX_PORTION = 0.75;
-
-const CoralLayer = ({
-	speed,
-	src,
-	id,
-	clientWidth,
-	clientHeight,
-	onHeightChange,
-}) => {
-	const layerRef = useRef(null);
-
-	useEffect(() => {
-		function updateHeight() {
-			if (!layerRef.current) return;
-			const rect = layerRef.current.getBoundingClientRect();
-			onHeightChange(id, rect.bottom);
-		}
-
-		updateHeight();
-
-		window.addEventListener("resize", updateHeight);
-		return () => window.removeEventListener("resize", updateHeight);
-	}, [id, onHeightChange]);
-
-	const end = speed * SCROLL_INCREMENT;
-	const aspectRatio = clientWidth / clientHeight;
-	const height =
-		(PARALLAX_PORTION * clientHeight * aspectRatio) / SVG_ASPECT_RATIO;
-
+const CoralParallaxLayer = ({ id }) => {
+	const arrIndex = id - 2;
 	return (
 		<Parallax
-			startScroll={0}
-			endScroll={height * 1.5}
-			translateY={[0, 20]}
-			className="coral"
 			id={`coral${id}`}
+			translateY={translatePositions[arrIndex]}
+			className="coral"
 			style={{
-				zIndex: 4 - speed,
+				position: "absolute",
+				zIndex: -id,
 			}}
 		>
 			<img
-				ref={layerRef}
-				src={src}
-				alt=""
+				src={`/assets/images/coral${id}.svg`}
+				alt={`Coral Layer ${id}`}
 				style={{
 					width: "100%",
 					height: "auto",
-					display: "block",
-					userSelect: "none",
-					pointerEvents: "none",
 				}}
 			/>
 		</Parallax>
 	);
 };
 
-const CoralParallaxSection = ({ clientWidth, clientHeight }) => {
-	const [containerHeight, setContainerHeight] = useState("100vh");
-	const largestHeightRef = useRef(0);
-
-	const [bottomPositions, setBottomPositions] = useState(
-		Array(coralImages.length).fill(0)
-	);
-
-	useEffect(() => {
-		const maxBottom = Math.max(...bottomPositions);
-		if (maxBottom > largestHeightRef.current) {
-			largestHeightRef.current = maxBottom;
-
-			const totalPx = Math.ceil(maxBottom) + 100;
-
-			const totalVh = (totalPx / window.innerHeight) * 100 + 100;
-			setContainerHeight(`${totalVh}vh`);
-		}
-	}, [bottomPositions]);
-
-	const handleHeightChange = (layerIndex, bottom) => {
-		setBottomPositions((prev) => {
-			const newPositions = [...prev];
-			if (Math.abs(newPositions[layerIndex] - bottom) < 5) {
-				return prev;
-			}
-			newPositions[layerIndex] = bottom;
-			return newPositions;
-		});
-	};
-
+const CoralParallaxSection = () => {
 	return (
 		<>
 			<div className="overflow-container">
 				<div className="background-container">
-					<Parallax
-						id="coral2"
-						translateY={[0, 35]}
-						className="coral"
-						style={{
-							position: "absolute",
-							zIndex: 2,
-						}}
-					>
-						<img
-							src="/assets/images/coral2.svg"
-							alt="Coral Layer 2"
-							style={{
-								width: "100%",
-								height: "auto",
-							}}
-						/>
-					</Parallax>
-
-					<Parallax
-						id="coral3"
-						translateY={[0, 65]}
-						className="coral"
-						style={{
-							position: "absolute",
-							zIndex: 1,
-						}}
-					>
-						<img
-							src="/assets/images/coral3.svg"
-							alt="Coral Layer 3"
-							style={{
-								width: "100%",
-								height: "auto",
-							}}
-						/>
-					</Parallax>
-
-					<Parallax
-						id="coral4"
-						translateY={[0, 110]}
-						className="coral"
-						style={{
-							position: "absolute",
-							zIndex: 0,
-						}}
-					>
-						<img
-							src="/assets/images/coral4.svg"
-							alt="Coral Layer 3"
-							style={{
-								width: "100%",
-								height: "auto",
-							}}
-						/>
-					</Parallax>
+					{[2, 3, 4].map((val) => (
+						<CoralParallaxLayer id={val} key={val} />
+					))}
 				</div>
 			</div>
 			<div className="coral-container">
@@ -176,7 +54,7 @@ const CoralParallaxSection = ({ clientWidth, clientHeight }) => {
 					}}
 				>
 					<img
-						src={darkBlueCoral}
+						src="/assets/images/coral1.svg"
 						alt="Dark Blue Coral"
 						style={{
 							width: "100%",
@@ -188,6 +66,7 @@ const CoralParallaxSection = ({ clientWidth, clientHeight }) => {
 					/>
 				</div>
 			</div>
+
 			<Container className="d-flex flex-column align-items-center content-container">
 				<Title />
 				{/** @todo: change links */}
