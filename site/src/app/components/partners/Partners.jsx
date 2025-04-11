8bf4226e63802b544ddfb47df9c8e331116b7383
Cindy <liangc30@uci.edu>
2025-04-10 17:23:42 -0700

    added partners section in home view; created partner bubble with basic styling and popping animation when center bubble is hovered over

--- site/src/app/components/partners/Partners.jsx
+++ site/src/app/components/partners/Partners.jsx
@@ -1,43 +1,45 @@
 import Partner from "./Partner";
 
-// import partners_title from "/assets/images/titles/vh-partners.png";
-
-// import acm from "/assets/images/2024-partners/acm.png";
-// import cyber from "/assets/images/2024-partners/cyber.png";
-// import design from "/assets/images/2024-partners/design.png";
-// import hack from "/assets/images/2024-partners/hack.png";
-// import icssc from "/assets/images/2024-partners/icssc.png";
-// import wics from "/assets/images/2024-partners/wics.png";
-// import data from "/assets/images/2024-partners/data.png";
+import placeholder from "/assets/images/shell.svg";
 
 import "./Partners.scss";
 
 const Partners = () => {
 	return (
 		<section id="partners">
-			<img className="section-title" src={partners_title} alt="Partners" />
 			<div id="partners-container">
-				<div className="logo-wrapper medium">
-					<Partner
-						imgId="design"
-						imgSrc={design}
-						url="https://designatuci.com/"
-					/>
-					<Partner imgId="acm" imgSrc={acm} url="https://www.acm-uci.org/" />
-					<Partner imgId="cyber" imgSrc={cyber} url="https://cyberuci.com/" />
-					<Partner
-						imgId="icssc"
-						imgSrc={icssc}
-						url="https://studentcouncil.ics.uci.edu/"
-					/>
-					<Partner imgId="wics" imgSrc={wics} url="https://wics.ics.uci.edu/" />
-					<Partner imgId="hack" imgSrc={hack} url="https://hack.ics.uci.edu/" />
-					<Partner
-						imgId="data"
-						imgSrc={data}
-						url="https://www.dataatuci.com/"
-					/>
+				{/* <h2 className="partner-title">Partners</h2> */}
+				<input type="button" value="<" />
+				<div className="bubble">
+					<div className="pop">
+						<div className="bckgrd">
+							<Partner
+								imgId="design"
+								imgSrc={placeholder}
+								url="https://designatuci.com/"
+							/>
+						</div>
+					</div>
 				</div>
+				<div className="bubble cur">
+					<div className="pop">
+						<Partner
+							imgId="design"
+							imgSrc={placeholder}
+							url="https://designatuci.com/"
+						/>
+					</div>
+				</div>
+				<div className="bubble">
+					<div className="pop">
+						<Partner
+							imgId="design"
+							imgSrc={placeholder}
+							url="https://designatuci.com/"
+						/>
+					</div>
+				</div>
+				<input type="button" value=">" />
 			</div>
 		</section>
 	);
--- site/src/app/views/home/Home.jsx
+++ site/src/app/views/home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "src/app/components/about/About";
 import FAQs from "src/app/components/faqs/FAQs";
+import Partners from "src/app/components/partners/Partners";
 import Hero from "src/app/components/hero/Hero";
 import BottomGraphic from "src/app/components/bottom-graphic/BottomGraphic";
 
@@ -27,6 +28,11 @@ const Home = () => {
 					<h2 className="section-title">FAQs</h2>
 					<FAQs />
 				</div>
+			</section>
+
+			{/* PARTNERS ************************/}
+			<section id="partner">
+				<Partners />
 			</section>
 
 			{/* BOTTOM GRAPHIC ******************/}
