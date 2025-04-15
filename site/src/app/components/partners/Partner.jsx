f5cd5b9b900cb9dc2711f1be4eff2bbda99c2e7c
Cindy <liangc30@uci.edu>
2025-04-15 14:57:15 -0700

    ran prettier

--- site/src/app/components/partners/Partner.jsx
+++ site/src/app/components/partners/Partner.jsx
@@ -2,11 +2,14 @@ const Partner = ({ imgId, imgSrc, addOn, desc }) => {
 	return (
 		<div id={addOn} className="bubble">
 			<div className="pop">
-				<img id={imgId} className="logo" src={imgSrc} alt={"Sponsor: " + imgId} />
+				<img
+					id={imgId}
+					className="logo"
+					src={imgSrc}
+					alt={"Sponsor: " + imgId}
+				/>
 			</div>
-			<div className="desc">
-				{desc}
-			</div>
+			<div className="desc">{desc}</div>
 		</div>
 	);
 };
--- site/src/app/components/partners/carousel.js
+++ site/src/app/components/partners/carousel.js
@@ -9,18 +9,18 @@ function getNext(cur, range) {
 export default function updateView(cur, dir) {
 	let partners = document.querySelectorAll(".bubble");
 
-    if (dir == -1) {
-        partners[getNext(cur, partners.length)].setAttribute("id", "");
-        partners[cur].setAttribute("id", "nxt");        
-        cur = getPrev(cur, partners.length);
-        partners[cur].setAttribute("id", "cur");        
-        partners[getPrev(cur, partners.length)].setAttribute("id", "prv");
+	if (dir == -1) {
+		partners[getNext(cur, partners.length)].setAttribute("id", "");
+		partners[cur].setAttribute("id", "nxt");
+		cur = getPrev(cur, partners.length);
+		partners[cur].setAttribute("id", "cur");
+		partners[getPrev(cur, partners.length)].setAttribute("id", "prv");
 	} else {
-        partners[getPrev(cur, partners.length)].setAttribute("id", "");
-        partners[cur].setAttribute("id", "prv");
-        cur = getNext(cur, partners.length);
-	    partners[cur].setAttribute("id", "cur");
-        partners[getNext(cur, partners.length)].setAttribute("id", "nxt");
+		partners[getPrev(cur, partners.length)].setAttribute("id", "");
+		partners[cur].setAttribute("id", "prv");
+		cur = getNext(cur, partners.length);
+		partners[cur].setAttribute("id", "cur");
+		partners[getNext(cur, partners.length)].setAttribute("id", "nxt");
 	}
 
 	return cur;
