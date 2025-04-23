6fadaebaa78c90880049a5e214f9e0040a60c5f8
chelseakari <sflake1225@gmail.com>
2025-04-22 21:41:29 -0700

    Hide the countdown timer before hacking starts

--- site/src/app/views/schedule/Schedule.jsx
+++ site/src/app/views/schedule/Schedule.jsx
@@ -1,31 +1,49 @@
+import { useEffect, useState } from "react";
 import { Countdown, ScheduleCard } from "src/app/components";
 
 import { Container } from "react-bootstrap";
 import StaticCoral from "src/app/components/static-coral/StaticCoral";
 import "./Schedule.scss";
 
-const HACKING_DEADLINE = "20 April 2025 09:00:00 PDT";
+// const HACKING_END = "25 May 2025 09:00:00 PDT";
+// const HACKING_START = new Date("23 May 2025 21:00:00 PDT");
+const HACKING_END = "25 April 2025 09:00:00 PDT"; // These are temporary test dates in order to see the timer
+const HACKING_START = new Date("22 April 2025 21:00:00 PDT");
+// TODO: Add bubbles with animations around the timer
 
-const Schedule = () => (
-	<div className="Schedule">
-		<section id="schedule-block">
-			<h1 className="schedule-title">SCHEDULE</h1>
-			<h4 className="schedule-hacking-ends-in">
-				All times in PDT. Hacking ends in:
-			</h4>
-			<div className="schedule-countdown">
-				<Container className="container-style">
-					<div className="countdown-content">
-						<Countdown date={HACKING_DEADLINE} />
-					</div>
-				</Container>
-			</div>
-			<ScheduleCard />
-		</section>
-		<StaticCoral />
-	</div>
-);
+const Schedule = () => {
+	const [showCountdown, setShowCountdown] = useState(false);
 
-// const Schedule = () => process.env.NEXT_PUBLIC_MAINTENANCE_MODE_SCHEDULE ? redirect("/") :<p>Schedule</p>;
+	useEffect(() => {
+		const now = new Date();
+		setShowCountdown(now >= HACKING_START);
+	}, []);
+
+	return (
+		<div className="Schedule">
+			<section id="schedule-block">
+				<h1 className="schedule-title">SCHEDULE</h1>
+
+				{showCountdown && (
+					<>
+						<h4 className="schedule-hacking-ends-in">
+							All times in PDT. Hacking ends in:
+						</h4>
+						<div className="schedule-countdown">
+							<Container className="container-style">
+								<div className="countdown-content">
+									<Countdown date={HACKING_END} />
+								</div>
+							</Container>
+						</div>
+					</>
+				)}
+
+				<ScheduleCard />
+			</section>
+			<StaticCoral />
+		</div>
+	);
+};
 
 export default Schedule;
