0144ffa3d77aa338d28f0a061bc0bb67922ce253
chelseakari <sflake1225@gmail.com>
2025-04-25 12:09:08 -0700

    Adjusted schedule and day button styling to fit on mobile devices.

--- site/src/app/views/schedule/Schedule.jsx
+++ site/src/app/views/schedule/Schedule.jsx
@@ -7,8 +7,8 @@ import "./Schedule.scss";
 
 // const HACKING_END = "25 May 2025 09:00:00 PDT";
 // const HACKING_START = new Date("23 May 2025 21:00:00 PDT");
-const HACKING_END = "25 April 2025 09:00:00 PDT"; // These are temporary test dates in order to see the timer
-const HACKING_START = new Date("22 April 2025 21:00:00 PDT");
+const HACKING_END = "27 April 2025 09:00:00 PDT"; // These are temporary test dates in order to see the timer
+const HACKING_START = new Date("24 April 2025 21:00:00 PDT");
 
 // TODO: FIX THE BUBBLE Y LOCATION CHANGING A LOT WHEN THE SCHEDULE LENGTH CHANGES.
 
