f69db7ef78d882e499c359c4d88afaf54b56ad1a
Cindy <liangc30@uci.edu>
2025-04-30 16:48:09 -0700

    modified index.ts order of schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,11 +1,12 @@
+import sponsors from "./sponsors";
 import event from "./event";
 import faqs from "./faqs";
 import resource from "./resource";
 import resourceCategory from "./resourceCategory";
 import resourceCategoryOrder from "./resourceCategoryOrder";
-import sponsors from "./sponsors";
 
 export const schemaTypes = [
+  sponsors,
   event,
   faqs,
   resource,
