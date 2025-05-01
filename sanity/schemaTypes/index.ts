2838b77d22d6742f3a39235f4f14a8224904fb2f
Cindy <liangc30@uci.edu>
2025-04-30 17:03:26 -0700

    changed order of schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,13 +1,14 @@
 import event from "./event";
+import partners from "./partners";
 import faqs from "./faqs";
 import resource from "./resource";
 import resourceCategory from "./resourceCategory";
 import resourceCategoryOrder from "./resourceCategoryOrder";
-import partners from "./partners"
 
 export const schemaTypes = [
   event,
-  partners, faqs,
+  partners, 
+  faqs,
   resource,
   resourceCategory,
   resourceCategoryOrder,
