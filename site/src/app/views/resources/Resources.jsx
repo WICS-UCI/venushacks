301c6bfb3a73d88172217ef614daf23ed135b3dd
ianbb <ianbbdai@gmail.com>
2025-05-14 16:13:30 -0700

    Some fixes to resources to use different components

--- site/src/app/views/resources/Resources.jsx
+++ site/src/app/views/resources/Resources.jsx
@@ -3,16 +3,9 @@ import { Tooltip, Footer } from "src/app/components";
 import { starterPackData } from "src/app/data/resources-info";
 import { Carousel, Stack, Card } from "react-bootstrap";
 import { Nav, Redirect } from "src/app/components";
-
-
-
 import useResources from "./useResources";
 
-// import next_icon from "/assets/images/next-icon.svg";
-// import prev_icon from "/assets/images/prev-icon.svg";
-
 import "./Resources.scss";
-
 
 function getWindowDimensions() {
 	const { innerWidth: width, innerHeight: height } = window;
@@ -23,12 +16,11 @@ function getWindowDimensions() {
 }
 
 const backgroundImages = [
-    "/assets/images/resources-card1.svg",
+	"/assets/images/resources-card1.svg",
 	"/assets/images/resources-card2.svg",
 	"/assets/images/resources-card3.svg",
 	"/assets/images/resources-card4.svg",
 ];
-
 
 export default function Resources() {
 	const { resources, isLoading, error } = useResources();
@@ -41,36 +33,51 @@ export default function Resources() {
 		<div className="Resources">
 			<div id="left-fishes" />
 			<div id="right-fishes" />
-			<div id="coral" />
-		<Nav />
-		<center><h1 style={{fontSize: "100px", textShadow: "#68B3D7 10px 10px 10px"}}>RESOURCES</h1></center>
+
+			<center>
+				<h1 style={{ fontSize: "100px", textShadow: "#68B3D7 10px 10px 10px" }}>
+					RESOURCES
+				</h1>
+			</center>
+			<img
+				src="/assets/images/resources-coral-top.svg"
+				alt="top coral"
+				id="resources-top-coral"
+			/>
+			<Nav />
 			<div className="resource-list">
-			{resources.order.map(({ _id, iconUrl, title, description, resources }, index) => (
-				<div key={_id} className="category">
-					<h3 style={{fontSize: "100px"}}> 
-						{title}
-					</h3>
-					<ResourceCarousel resources={resources} backgroundImage={backgroundImages[index % backgroundImages.length]} className="resource-carousel"/>
-				</div>
-			))}
+				{resources.order.map(
+					({ _id, iconUrl, title, description, resources }, index) => (
+						<div key={_id} className="category">
+							<h3 style={{ fontSize: "100px" }}>{title}</h3>
+							<ResourceCarousel
+								resources={resources}
+								backgroundImage={
+									backgroundImages[index % backgroundImages.length]
+								}
+								className="resource-carousel"
+							/>
+						</div>
+					)
+				)}
 			</div>
 		</div>
 	);
 }
 
-
 function ResourceCarousel({ resources, backgroundImage }) {
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [isTransitioning, setIsTransitioning] = useState(true);
 	const totalSlides = resources.length;
-	
 
 	const nextResource = () => {
 		setCurrentIndex((prevIndex) => (prevIndex + 1) % resources.length);
 	};
 
 	const prevResource = () => {
-		setCurrentIndex((prevIndex) => (prevIndex - 1 + resources.length) % resources.length);
+		setCurrentIndex(
+			(prevIndex) => (prevIndex - 1 + resources.length) % resources.length
+		);
 	};
 
 	// Transition back to first card
@@ -84,7 +91,9 @@ function ResourceCarousel({ resources, backgroundImage }) {
 	}, [currentIndex, totalSlides]);
 
 	return (
-		<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+		<div
+			style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+		>
 			{/* Carousel Section */}
 			<div className="carousel-container">
 				<button className="carousel-button left" onClick={prevResource}>
@@ -99,27 +108,33 @@ function ResourceCarousel({ resources, backgroundImage }) {
 							transition: "transform 0.4s ease-in-out",
 						}}
 					>
-						{resources.map(({ _id, resourceIconUrl, title, link, description }) => (
-							<div
-								key={_id}
-								className="resource-card"
-								style={{
-									backgroundImage: `url(${backgroundImage})`,
-									backgroundSize: "cover",
-								}}
-							>
-								<a
-									href={link}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="card-link"
+						{resources.map(
+							({ _id, resourceIconUrl, title, link, description }) => (
+								<div
+									key={_id}
+									className="resource-card"
+									style={{
+										// backgroundImage: `url(${backgroundImage})`,
+										backgroundSize: "cover",
+									}}
 								>
-									<img src={resourceIconUrl} alt={title} className="resource-icon" />
-									<h4 className="resource-title">{title}</h4>
-									<p className="resource-description">{description}</p>
-								</a>
-							</div>
-						))}
+									<a
+										href={link}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="card-link"
+									>
+										<img
+											src={resourceIconUrl}
+											alt={title}
+											className="resource-icon"
+										/>
+										<h4 className="resource-title">{title}</h4>
+										<p className="resource-description">{description}</p>
+									</a>
+								</div>
+							)
+						)}
 					</div>
 				</div>
 
