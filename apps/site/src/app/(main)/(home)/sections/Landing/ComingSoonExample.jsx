945d6de66a99ec72481eb97c301f86c03fb5d114
Aariel Abaincia <abaincia.uci.edu>
2026-02-03 23:21:02 -0800

    coming soon page example

---
+++ apps/site/src/app/(main)/(home)/sections/Landing/ComingSoonExample.jsx
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/Container";
+
+const ComingSoonExample = () => {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center vh-100"
+    >
+      <h1 style={{ color: "#6b21a8" }}>Coming Soon ✨</h1>
+    </Container>
+  );
+};
+
+export default ComingSoonExample;
