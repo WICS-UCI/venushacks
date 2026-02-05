34e2bfa1dd2e45a1c64c5bc17fcc3a359a2953ec
Noah Kim <101167833+noahk004@users.noreply.github.com>
2026-02-04 22:30:57 -0800

    Update apps/site/src/app/layout.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { PropsWithChildren } from "react";
 // This line will prevent statically generating the admin pages upon deploying.
 export const dynamic = "force-dynamic";
 
-
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="en">
