7ece733c21b958dd7a977ca83e89bcc177ad4299
Noah Kim <noahmk5002@gmail.com>
2026-02-04 22:20:51 -0800

    Add force-dynamic to layout

--- apps/site/src/app/admin/layout.tsx
+++ apps/site/src/app/admin/layout.tsx
@@ -6,7 +6,4 @@ export const metadata: Metadata = {
 	title: "Admin | IrvineHacks 2025",
 };
 
-// This line will prevent statically generating the admin pages upon deploying.
-export const dynamic = "force-dynamic";
-
 export { default as default } from "./layout/AdminLayout";
