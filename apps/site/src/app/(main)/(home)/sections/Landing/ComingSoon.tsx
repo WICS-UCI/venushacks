9a1d924f8aba71bab4e59eb564304f717aa0dc71
Jonathan Chau <chaujt1@uci.edu>
2026-02-07 17:12:43 -0800

    Structuring fixed div for social links.

--- apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
@@ -8,16 +8,67 @@ import ApplicationsButtonImage from "@/assets/icons/application-warning.svg";
 
 import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
 
-
 import styles from "./Landing.module.css";
 
 const ComingSoon = () => {
-
 	return (
 		<>
+			{/* Fixed social links */}
+			<div className="fixed top-4 right-4 z-10">
+				<ul className="flex items-center gap-2">
+					<li>
+						<a
+							href="https://www.tiktok.com/@venushacksuci"
+							target="_blank"
+							rel="noopener noreferrer"
+							aria-label="VenusHacks on Tiktok"
+							className="flex h-11 w-11 items-center justify-center"
+						>
+							{/* TIKTOK SVG */}
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.instagram.com/venushacksuci/"
+							target="_blank"
+							rel="noopener noreferrer"
+							aria-label="VenusHacks on Instagram"
+							className="flex h-11 w-11 items-center justify-center"
+						>
+							{/* INSTAGRAM SVG */}
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.linkedin.com/company/venushacks/"
+							target="_blank"
+							rel="noopener noreferrer"
+							aria-label="VenusHacks on LinkedIn"
+							className="flex h-11 w-11 items-center justify-center"
+						>
+							{/* LinkedIn SVG */}
+						</a>
+					</li>
+					<li>
+						<a
+							href="mailto:venushacks.uci@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							aria-label="Email VenusHacks"
+							className="flex h-11 w-11 items-center justify-center"
+						>
+							{/* MAIL SVG */}
+						</a>
+					</li>
+				</ul>
+			</div>
 			<div className="min-h-screen flex items-center justify-center">
-                <h1 className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}>COMING SOON</h1>
-            </div>
+				<h1
+					className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}
+				>
+					COMING SOON
+				</h1>
+			</div>
 		</>
 	);
 };
