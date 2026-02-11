ba5d2b5417e28046360ebc59bf6f4a9ecccb572b
Mehek Bhatnagar <mehekbhatnagar3@gmail.com>
2026-02-10 22:18:32 -0800

    updated coming soon page background

--- apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
@@ -3,8 +3,8 @@
 import { useRef } from "react";
 import Image from "next/image";
 
-import LandingBackground from "@/assets/backgrounds/landing-background.png";
-import ApplicationsButtonImage from "@/assets/icons/application-warning.svg";
+import LandingBackgroundGrass from "@/assets/backgrounds/coming-soon-background-grass.svg";
+import CloudGroup from "@/assets/backgrounds/coming-soon-cloud-group.svg";
 
 import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
 
@@ -12,14 +12,34 @@ import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
 import styles from "./Landing.module.css";
 
 const ComingSoon = () => {
+  return (
+    <>
+      <div
+        className="min-h-screen flex items-center justify-center"
+        style={{ backgroundColor: '#B2EEE7' }}
+      >
+		<div className={styles.cloudsWrapper}>
+		<Image
+			src={CloudGroup}
+			alt="Clouds"
+			className={styles.cloudGroup}
+			width={1600}   // adjust width as needed
+			height={400}  // adjust height as needed
+			priority
+		/>
+		</div>
+        <h1 className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}>
+          COMING SOON
+        </h1>
+      </div>
 
-	return (
-		<>
-			<div className="min-h-screen flex items-center justify-center">
-                <h1 className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}>COMING SOON</h1>
-            </div>
-		</>
-	);
+      <Image
+        src={LandingBackgroundGrass}
+        alt="Landing Background Grass"
+        className={styles.landingImage}
+      />
+    </>
+  );
 };
 
 export default ComingSoon;
