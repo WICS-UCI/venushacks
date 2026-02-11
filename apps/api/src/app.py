230d2a033a237c0f0246c156384f1554c15de927
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2026-02-11 15:30:33 -0800

    feat: add mailing list router, tests, and update app and mongodb_handler

--- apps/api/src/app.py
+++ apps/api/src/app.py
@@ -3,7 +3,7 @@ import os
 
 from fastapi import FastAPI
 
-from routers import admin, director, guest, saml, user
+from routers import admin, director, guest, mailing_list, saml, user
 from middleware.hackathon_context_middleware import HackathonContextMiddleware
 
 logging.basicConfig(level=logging.INFO)
@@ -24,6 +24,7 @@ app.include_router(guest.router, prefix="/guest", tags=["guest"])
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
 app.include_router(director.router, prefix="/director", tags=["director"])
+app.include_router(mailing_list.router, prefix="/mailing-list", tags=["mailing-list"])
 
 app.add_middleware(HackathonContextMiddleware)
 
