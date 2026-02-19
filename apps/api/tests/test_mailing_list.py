660db6a35116643e8a730fe977b0035515c02272
Jonathan Chau <jonathan.chau06ios@gmail.com>
2026-02-19 14:13:34 -0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/api/tests/test_mailing_list.py
+++ apps/api/tests/test_mailing_list.py
@@ -75,8 +75,7 @@ def test_post_mailing_list_invalid_body_returns_error(
 
     assert res.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert (
-        res.json()["detail"]
-        == "Invalid body. Expected JSON with fields: "
+        res.json()["detail"] == "Invalid body. Expected JSON with fields: "
         "email (string), timeSubmitted (string)."
     )
     mock_mongodb_handler_insert.assert_not_awaited()
