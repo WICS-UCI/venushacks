e3c2fb9659997a716128e4ef711712188d13c6d4
Noah Kim <noahmk5002@gmail.com>
2026-02-19 21:35:05 -0800

    Revert changes made for testing

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import { PropsWithChildren } from "react";
 
-// This line will prevent statically generating the admin pages upon deploying.
-export const dynamic = "force-dynamic";
-
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="en">
