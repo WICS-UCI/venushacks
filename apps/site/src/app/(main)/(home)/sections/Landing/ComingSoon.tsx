244de555312aee33cf89765b75d1b41c8cb02cd6
Noah Kim <noahmk5002@gmail.com>
2026-02-26 08:56:08 -0800

    Add prettier changes

--- apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
@@ -6,7 +6,9 @@ const ComingSoon = () => {
 	return (
 		<>
 			<div className="min-h-screen flex items-center justify-center">
-				<h1 className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}>
+				<h1
+					className={`${styles.headingDropShadow} font-heading text-4xl md:text-7xl lg:text-5xl mb-12 text-center`}
+				>
 					COMING SOON
 				</h1>
 			</div>
