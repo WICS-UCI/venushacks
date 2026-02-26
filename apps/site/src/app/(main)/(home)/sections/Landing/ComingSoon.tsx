bf0fda16c96df84eb00b871f47a8bc41890e7a67
Noah Kim <noahmk5002@gmail.com>
2026-02-26 13:05:22 -0800

    Prettier changes, refactor background code

--- apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/ComingSoon.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { useState, useEffect, FormEvent } from "react";
-import Image from "next/image";
 
 import SocialLinks from "./SocialLinks";
-
-import LandingBackgroundGrass from "@/assets/backgrounds/coming-soon-background-grass.svg";
-import CloudGroup from "@/assets/backgrounds/coming-soon-clouds.svg";
-
-import styles from "./Landing.module.css";
+import ComingSoonBackground from "./ComingSoonBackground"
 
 const ComingSoon = () => {
 	const [email, setEmail] = useState("");
@@ -60,27 +55,8 @@ const ComingSoon = () => {
 
 	return (
 		<>
+			<ComingSoonBackground />
 			<SocialLinks />
-			<div className="relative min-h-screen md:h-screen overflow-hidden bg-[#B2EEE7]">
-				{/* Clouds */}
-				<div className={`${styles.cloudTrack} absolute top-[5%] md:top-0 left-0`}>
-					<Image src={CloudGroup} alt="Clouds" className="w-[180vw] md:w-screen h-auto flex-shrink-0" />
-					<Image src={CloudGroup} alt="Clouds" className="w-[180vw] md:w-screen h-auto flex-shrink-0" />
-				</div>
-
-				{/* Heading */}
-				<h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-				text-center font-heading text-4xl md:text-7xl lg:text-7xl drop-shadow-xl z-10">
-					COMING SOON
-				</h1>
-
-				{/* Grass at the bottom */}
-				<Image
-					src={LandingBackgroundGrass}
-					alt="Landing Background Grass"
-					className="absolute bottom-8 md:bottom-[-5%] w-full scale-150 md:scale-100 left-1/2 -translate-x-1/2"
-				/>
-			</div>
 			<div className="min-h-screen flex flex-col items-center justify-center px-4 py-8 relative -mt-8 md:-mt-12">
 				{/* Coming Soon Text */}
 				<p className="font-sniglet font-normal text-coming-soon text-dark-text mb-1 md:mb-2 text-center">
