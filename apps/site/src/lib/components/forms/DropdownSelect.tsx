1002c3ffa5de473b654c0a669618cf33095b8cba
Yousef <theyousefkhan@gmail.com>
2026-03-04 05:39:26 -0800

    Updated form components

--- apps/site/src/lib/components/forms/DropdownSelect.tsx
+++ apps/site/src/lib/components/forms/DropdownSelect.tsx
@@ -48,7 +48,7 @@ export default function DropdownSelect({
 	isRequired = true,
 	placeholder = "",
 }: SelectProps) {
-	const [value, setValue] = useState(defaultValue);
+	const [value, setValue] = useState("");
 
 	return (
 		<div className={containerClass}>
