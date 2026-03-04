dda200247c7b254c2252dcab04611ac5ce6eaaef
Yousef <theyousefkhan@gmail.com>
2026-03-04 05:58:34 -0800

    Part 2

--- apps/site/src/app/(main)/mentor/Form/MentorShortAnswers.tsx
+++ apps/site/src/app/(main)/mentor/Form/MentorShortAnswers.tsx
@@ -2,38 +2,39 @@ import Textfield from "@/lib/components/forms/Textfield";
 
 export default function ShortAnswers() {
 	return (
-		<div className="flex flex-col gap-5 w-11/12">
+		<div className="flex flex-col w-11/12 gap-5">
 			<p className="text-4xl m-0 font-bold max-[700px]:text-3xl">
-				Profile Information
+				II. Short Answers
 			</p>
 			<Textfield
-				name="mentor_prev_experience_saq1"
-				labelText="Have you participated or mentored at a hackathon before? If so, please list which ones. e.g. IrvineHacks 2024 (Hacker), ZotHacks 2024 (Mentor)"
+				name="mentor_why_saq1"
+				labelText="Why do you want to be a mentor for VenusHacks? What are you hoping to gain from this experience?"
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+				maxLength={1500}
+			/>
+			<Textfield
+				name="mentor_inclusive_saq2"
+				labelText="How do you hope to contribute to VenusHacks' inclusive environment?"
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+				maxLength={1500}
+			/>
+			<Textfield
+				name="mentor_availability_saq3"
+				labelText="Are you available to commit to the entire duration of VenusHacks 2026? If not, please specify your availability."
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+				maxLength={1500}
+			/>
+			<Textfield
+				name="mentor_questions_saq4"
+				labelText="Questions, comments, concerns?"
 				containerClass="flex flex-col w-full"
 				isRequired={false}
-				maxLength={1500}
-			/>
-			<Textfield
-				name="mentor_interest_saq2"
-				labelText="Why are you interested in being a mentor for IrvineHacks 2025? (100+ words recommended)"
-				containerClass="flex flex-col w-full"
-				isRequired={true}
-				maxLength={1500}
-			/>
-			<Textfield
-				name="mentor_team_help_saq3"
-				labelText="How would you go about helping a team that is struggling with a bug?"
-				containerClass="flex flex-col w-full"
-				isRequired={true}
-				maxLength={1500}
-			/>
-			<Textfield
-				name="mentor_team_help_saq4"
-				labelText="How would you go about helping a team that is struggling to work together?"
-				containerClass="flex flex-col w-full"
-				isRequired={true}
 				maxLength={1500}
 			/>
 		</div>
 	);
 }
+
