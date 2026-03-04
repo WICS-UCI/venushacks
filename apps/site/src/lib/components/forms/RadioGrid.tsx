283758631f13cb10c4b2d5b00b5d54fed87bc076
Yousef Khan <72176181+YKawesome@users.noreply.github.com>
2026-03-04 06:08:42 -0800

    Update apps/site/src/lib/components/forms/RadioGrid.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/lib/components/forms/RadioGrid.tsx
+++ apps/site/src/lib/components/forms/RadioGrid.tsx
@@ -31,7 +31,7 @@ export default function RadioGrid({
 				</p>
 			)}
 			<div className="w-full overflow-x-auto border border-gray-300 shadow-md rounded-xl">
-			<table className="min-w-full text-sm">
+				<table className="min-w-full text-sm">
 					<thead>
 						<tr className="border-b border-gray-200">
 							<th className="w-1/3 px-4 py-3 font-medium text-left text-gray-700" />
