2f1cbe10e39165929a413bcff19ad053e4f11909
Noah Kim <noahmk5002@gmail.com>
2026-03-05 15:27:00 -0800

    Formatting fix

--- apps/api/index.py
+++ apps/api/index.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from app import app as api
 from auth.guest_auth import AUTH_KEY_SALT
 from auth.user_identity import JWT_SECRET
-from services.docusign_handler import DOCUSIGN_HMAC_KEY
+# from services.docusign_handler import DOCUSIGN_HMAC_KEY
 
 if not JWT_SECRET:
     raise RuntimeError("JWT_SECRET is not defined")
