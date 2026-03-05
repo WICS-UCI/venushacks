ca8c2f20b776eabe7f6ad63fed0438cc3c958074
Noah Kim <noahmk5002@gmail.com>
2026-03-04 23:14:59 -0800

    Minor refactoring to fields with other

--- apps/api/tests/test_user_mentor_apply.py
+++ apps/api/tests/test_user_mentor_apply.py
@@ -265,8 +265,9 @@ def test_mentor_application_data_with_other_throws_422(
 ) -> None:
     mock_mongodb_handler_retrieve_one.return_value = None
     contains_other = copy.deepcopy(SAMPLE_APPLICATION)
-    contains_other["experienced_technologies"]\
-        .append("other")  # type: ignore[attr-defined]
+    contains_other["experienced_technologies"].append(
+        "other"
+    )  # type: ignore[attr-defined]
     res = client.post("/mentor", data=contains_other, files=SAMPLE_FILES)
     assert res.status_code == 422
 
