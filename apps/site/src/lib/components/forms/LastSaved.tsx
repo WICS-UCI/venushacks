9fd57cc79703bd272585cc7ff4cf282897cc3885
Jonathan Chau <jonathan.chau06ios@gmail.com>
2026-03-05 14:03:12 -0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/lib/components/forms/LastSaved.tsx
+++ apps/site/src/lib/components/forms/LastSaved.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 interface LastSavedProps {
-    lastSaved: Date | null;
+	lastSaved: Date | null;
 }
 
-// Track saved time with useState in a parent Component. 
+// Track saved time with useState in a parent Component.
 export default function LastSaved({ lastSaved }: LastSavedProps) {
-    const formatted = lastSaved
-        ? lastSaved.toLocaleDateString("en-US", {
-              month: "2-digit",
-              day: "2-digit",
-              year: "numeric",
-          }) +
-          " at " +
-          lastSaved.toLocaleTimeString("en-US", {
-              hour: "2-digit",
-              minute: "2-digit",
-              second: "2-digit",
-              hour12: false,
-          })
-        : "mm/dd/yyyy at 00:00:00";
+	const formatted = lastSaved
+		? lastSaved.toLocaleDateString("en-US", {
+				month: "2-digit",
+				day: "2-digit",
+				year: "numeric",
+		  }) +
+		  " at " +
+		  lastSaved.toLocaleTimeString("en-US", {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit",
+				hour12: false,
+		  })
+		: "mm/dd/yyyy at 00:00:00";
 
 	return (
 		<div className="mt-8 flex items-center justify-between">
