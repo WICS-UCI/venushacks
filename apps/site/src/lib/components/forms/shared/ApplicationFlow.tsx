9869fb6afce916c5c3836f177885a84edeb78ab8
Sarah Tan <61632695+sarahtan0@users.noreply.github.com>
2026-03-07 13:09:03 -0800

    Update apps/site/src/lib/components/forms/shared/ApplicationFlow.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/lib/components/forms/shared/ApplicationFlow.tsx
+++ apps/site/src/lib/components/forms/shared/ApplicationFlow.tsx
@@ -70,9 +70,13 @@ export default function ApplicationFlow({
 						{children[pageIndex]}
 						<div className="mt-8 flex justify-between">
 							{!isFirstPage() && (
-								<button className="px-3 py-2 bg-[#FFECEC80] border border-[#CF6868] rounded-full w-40 text-button-text font-bold"
-								type="button"
-								onClick={() => setPageIndex(pageIndex-1)}>← 	Back</button>
+								<button
+									className="px-3 py-2 bg-[#FFECEC80] border border-[#CF6868] rounded-full w-40 text-button-text font-bold"
+									type="button"
+									onClick={() => setPageIndex(pageIndex - 1)}
+								>
+									← Back
+								</button>
 							)}
 							{isLastPage() ? (
 								<Button
