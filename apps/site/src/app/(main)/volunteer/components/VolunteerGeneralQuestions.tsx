59a80e1401a66f5fe713ebbddab1aeda5a241482
Sarah Tan <61632695+sarahtan0@users.noreply.github.com>
2026-03-07 13:07:28 -0800

    Update apps/site/src/app/(main)/volunteer/components/VolunteerGeneralQuestions.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/app/(main)/volunteer/components/VolunteerGeneralQuestions.tsx
+++ apps/site/src/app/(main)/volunteer/components/VolunteerGeneralQuestions.tsx
@@ -7,31 +7,29 @@ const yesNoValues = [
                 ];
 
 export default function VolunteerGeneralQuestions() {
-    return (
-        <div className="flex flex-col items-start gap-5">
-            <div className="text-xl font-semibold">II. General Questions</div>
-            <DropdownSelectYesFRQ
-                name="prior_experience"
-                labelText="Any prior hackathon volunteer experience (ex. VH 2025)?"
-                containerClass="w-full"
-                values={yesNoValues}
-            />
-            <SimpleCheckBox
-                name="transportation"
-                labelText="I acknowledge that I am responsible for my own transportation to UC Irvine and overnight stay between event days."
-                containerClass="w-full"
-                isRequired={true}
-                labelClass=""
-
-            />
-            <SimpleCheckBox
-                name="availability_confirmation"
-                labelText="I will be available to volunteer a combined amount of 5 hours minimum across Friday, May 15th, through May 17th, Sunday."
-                containerClass="w-full"
-                isRequired={true}
-                labelClass=""
-
-            />
-        </div>
-    );
+	return (
+		<div className="flex flex-col items-start gap-5">
+			<div className="text-xl font-semibold">II. General Questions</div>
+			<DropdownSelectYesFRQ
+				name="prior_experience"
+				labelText="Any prior hackathon volunteer experience (ex. VH 2025)?"
+				containerClass="w-full"
+				values={yesNoValues}
+			/>
+			<SimpleCheckBox
+				name="transportation"
+				labelText="I acknowledge that I am responsible for my own transportation to UC Irvine and overnight stay between event days."
+				containerClass="w-full"
+				isRequired={true}
+				labelClass=""
+			/>
+			<SimpleCheckBox
+				name="availability_confirmation"
+				labelText="I will be available to volunteer a combined amount of 5 hours minimum across Friday, May 15th, through May 17th, Sunday."
+				containerClass="w-full"
+				isRequired={true}
+				labelClass=""
+			/>
+		</div>
+	);
 }
