b255ff62b575bf0ab46ca9fcd994297221d8e28c
Noah Kim <101167833+noahk004@users.noreply.github.com>
2026-03-08 18:48:03 -0700

    Update apps/site/src/lib/components/forms/LastSaved.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/lib/components/forms/LastSaved.tsx
+++ apps/site/src/lib/components/forms/LastSaved.tsx
@@ -8,17 +8,17 @@ interface LastSavedProps {
 export default function LastSaved({ lastSaved }: LastSavedProps) {
 	const formatted = lastSaved
 		? lastSaved.toLocaleDateString("en-US", {
-			month: "2-digit",
-			day: "2-digit",
-			year: "numeric",
-		}) +
-		" at " +
-		lastSaved.toLocaleTimeString("en-US", {
-			hour: "2-digit",
-			minute: "2-digit",
-			second: "2-digit",
-			hour12: false,
-		})
+				month: "2-digit",
+				day: "2-digit",
+				year: "numeric",
+		  }) +
+		  " at " +
+		  lastSaved.toLocaleTimeString("en-US", {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit",
+				hour12: false,
+		  })
 		: "mm/dd/yyyy at 00:00:00";
 
 	return (
