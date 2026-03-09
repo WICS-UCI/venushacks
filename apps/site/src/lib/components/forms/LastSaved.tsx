426faab93b66382095b896dc1edb6e00e0edc4d5
Noah Kim <noahmk5002@gmail.com>
2026-03-08 18:56:52 -0700

    Refactor last saved component

--- apps/site/src/lib/components/forms/LastSaved.tsx
+++ apps/site/src/lib/components/forms/LastSaved.tsx
@@ -1,24 +1,29 @@
 "use client";
 
+function formatAutoSaveDate(date: Date): string {
+	const s1 = date.toLocaleDateString("en-US", {
+		month: "2-digit",
+		day: "2-digit",
+		year: "numeric",
+	});
+
+	const s2 = date.toLocaleTimeString("en-US", {
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		hour12: false,
+	});
+
+	return `${s1} at ${s2}`;
+}
+
 interface LastSavedProps {
 	lastSaved: Date | null;
 }
 
 // Track saved time with useState in a parent Component.
 export default function LastSaved({ lastSaved }: LastSavedProps) {
-	const formatted = lastSaved
-		? lastSaved.toLocaleDateString("en-US", {
-				month: "2-digit",
-				day: "2-digit",
-				year: "numeric",
-			}) +
-			" at " +
-			lastSaved.toLocaleTimeString("en-US", {
-				hour: "2-digit",
-				minute: "2-digit",
-				second: "2-digit",
-				hour12: false,
-			})
+	const formatted = lastSaved ? formatAutoSaveDate(lastSaved)
 		: "mm/dd/yyyy at 00:00:00";
 
 	return (
