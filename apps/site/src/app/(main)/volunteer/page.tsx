e5e4b5ef2b16ae6a276ce57755c815673e4264b7
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2026-03-09 15:24:52 -0700

    fix: VenusHacks should be spelled without a space

--- apps/site/src/app/(main)/volunteer/page.tsx
+++ apps/site/src/app/(main)/volunteer/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Volunteer Application | Venus Hacks 2026",
+	title: "Volunteer Application | VenusHacks 2026",
 };
 
 export { default as default } from "./Volunteer";
