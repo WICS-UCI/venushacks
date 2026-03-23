194949288eb92c21ca7777bc98a7eb3a3b466274
Jonathan Chau <jonathan.chau06ios@gmail.com>
2026-03-22 23:29:30 -0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/app/(main)/(home)/sections/Organizers/Organizers.tsx
+++ apps/site/src/app/(main)/(home)/sections/Organizers/Organizers.tsx
@@ -57,10 +57,10 @@ export default function Organizers() {
 		activeTab === "Board"
 			? organizers.filter(
 					(o) => o.role === "Co-President" || o.role === "Co-Chair",
-			)
+			  )
 			: organizers.filter(
 					(o) => o.department === activeTab && o.role === "Organizer",
-			);
+			  );
 
 	if (isLoading) {
 		return null;
--- apps/site/src/app/(main)/(home)/sections/Organizers/getOrganizers.ts
+++ apps/site/src/app/(main)/(home)/sections/Organizers/getOrganizers.ts
@@ -18,7 +18,9 @@ const Organizers = SanityDocument.extend({
 });
 
 export const getOrganizers = cache(async () => {
-	const result = await client.fetch("*[_type == 'organizers' && _id == 'organizers'][0]");
+	const result = await client.fetch(
+		"*[_type == 'organizers' && _id == 'organizers'][0]",
+	);
 
 	if (!result) {
 		console.error(
