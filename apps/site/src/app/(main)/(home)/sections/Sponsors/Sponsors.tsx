3f112ea677cb88d706e5c1aa74fcbdcab8b8ad3f
Sarah <tans9@uci.edu>
2026-03-29 15:07:57 -0700

    added assets

--- apps/site/src/app/(main)/(home)/sections/Sponsors/Sponsors.tsx
+++ apps/site/src/app/(main)/(home)/sections/Sponsors/Sponsors.tsx
@@ -1,6 +1,8 @@
 import { getSponsors } from "./getSponsors";
 import SponsorTier from "./components/SponsorTier/SponsorTier";
 import styles from "./Sponsors.module.scss";
+import { motion } from "framer-motion";	
+import blueberry from "./assets/blueberry_ant.svg";
 
 // no sponsors under bronze for IH 2025
 const TIERS = [
@@ -22,6 +24,17 @@ const Sponsors = async () => {
 			>
 				Sponsors
 			</h2>
+			<motion.div
+				className="flex w-max"
+				animate={{ x: ["0%", "-50%"] }}
+				transition={{
+				repeat: Infinity,
+				duration: 5,
+				ease: "linear",
+				}}
+			>
+				<img src={blueberry} className="w-[300px] shrink-0" />
+			</motion.div>
 
 		</section>
 	);
