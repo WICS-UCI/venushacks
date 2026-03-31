188a9de2fb892dcec6b365660af275af0262793a
Noah Kim <noahmk5002@gmail.com>
2026-03-31 15:12:16 -0700

    Minor change to pytest

--- apps/api/tests/test_user.py
+++ apps/api/tests/test_user.py
@@ -16,9 +16,14 @@ client = TestClient(app, follow_redirects=False)
 
 # def test_login_as_uci_redirects_to_saml() -> None:
 #     """Tests that logging in with UCI email redirects to SAML for UCI SSO"""
-#     res = client.post("/login", data={"email": "hack@uci.edu"}, follow_redirects=False)
+#     res = client.post(
+#         "/login",
+#         data={"email": "hack@uci.edu"},
+#         follow_redirects=False,
+#     )
 #     assert res.status_code == status.HTTP_303_SEE_OTHER
 #     assert res.headers["location"] == "/api/saml/login?return_to=%2Fportal"
+
 
 def test_login_as_non_uci_redirects_to_guest_login() -> None:
     """Test that logging in with a non-UCI email redirects to guest login endpoint."""
