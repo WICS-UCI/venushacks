7d102f224efb2168338348672bcb57df7ff53849
Noah Kim <noahmk5002@gmail.com>
2026-04-01 13:23:49 -0700

    Apply formatting changes

--- apps/api/src/routers/user.py
+++ apps/api/src/routers/user.py
@@ -216,15 +216,11 @@ async def _apply_flow(
         except TypeError as err:
             log.info("An error occurred while submitting an application for %s:", user)
             log.info("Message: %s", err)
-            raise HTTPException(
-                status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, err
-            )
+            raise HTTPException(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, err)
         except ValueError as err:
             log.info("An error occurred while submitting an application for %s:", user)
             log.info("Message: %s", err)
-            raise HTTPException(
-                status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, err
-            )
+            raise HTTPException(status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, err)
         except RuntimeError as err:
             log.error("During user %s apply, resume upload: %s", user.uid, err)
             raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
