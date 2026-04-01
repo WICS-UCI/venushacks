e521a23df4c1113e59aabd52391d1757abe7eaa7
Noah Kim <noahmk5002@gmail.com>
2026-04-01 13:44:20 -0700

    Minor syntax change

--- apps/api/src/routers/user.py
+++ apps/api/src/routers/user.py
@@ -214,13 +214,15 @@ async def _apply_flow(
                 resume,
             )
         except TypeError as err:
-            log.info("An error occurred while submitting an application for %s: %s",
+            log.info(
+                "An error occurred while submitting an application for %s: %s",
                 user,
                 err,
             )
             raise HTTPException(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, err)
         except ValueError as err:
-            log.info("An error occurred while submitting an application for %s: %s",
+            log.info(
+                "An error occurred while submitting an application for %s: %s",
                 user,
                 err,
             )
