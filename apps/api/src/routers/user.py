8e382586b0036ab9c4bd7c9b8a75722c6dcbd472
Noah Kim <noahmk5002@gmail.com>
2026-04-01 13:32:22 -0700

    Minor syntax change

--- apps/api/src/routers/user.py
+++ apps/api/src/routers/user.py
@@ -214,12 +214,10 @@ async def _apply_flow(
                 resume,
             )
         except TypeError as err:
-            log.info("An error occurred while submitting an application for %s:", user)
-            log.info(f"Message: {err}")
+            log.info("An error occurred while submitting an application for %s: %s", user, err)
             raise HTTPException(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, err)
         except ValueError as err:
-            log.info("An error occurred while submitting an application for %s:", user)
-            log.info(f"Message: {err}")
+            log.info("An error occurred while submitting an application for %s: %s", user, err)
             raise HTTPException(status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, err)
         except RuntimeError as err:
             log.error("During user %s apply, resume upload: %s", user.uid, err)
