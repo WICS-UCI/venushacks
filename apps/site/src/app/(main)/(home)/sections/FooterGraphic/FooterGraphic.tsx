44274f00c170ecc252830203fbd8daff2c679586
Sarah <tans9@uci.edu>
2026-04-01 13:02:41 -0700

    fixed formatting for classNames

--- apps/site/src/app/(main)/(home)/sections/FooterGraphic/FooterGraphic.tsx
+++ apps/site/src/app/(main)/(home)/sections/FooterGraphic/FooterGraphic.tsx
@@ -33,28 +33,20 @@ const FooterGraphic = () => {
 				<Image
                     src={shadow}
 					alt="Shadows"
-					className={
-					    "absolute left-[49%] top-[67%] w-[90%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[49%] top-[67%] w-[90%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={rainbow}
 					alt="Rainbow"
-					className={
-					    "absolute left-[40%] top-[35%] w-[50%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[40%] top-[35%] w-[50%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={pond}
 					alt="Pond"
-					className={
-						"absolute left-1/2 top-1/2  w-[80%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-1/2 top-1/2  w-[80%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<motion.div
-					className={
-						"absolute left-[50%] top-[34%] w-[17%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[50%] top-[34%] w-[17%] -translate-x-1/2 -translate-y-1/2"
 					animate={{ y: [0, -12, 0] }}
 					transition={{
 						duration: 3,
@@ -66,38 +58,28 @@ const FooterGraphic = () => {
 					<Image
 						src={apple}
 						alt="Apple"
-						className={
-							"absolute left-[50%] top-[13%] w-[22%] -translate-x-1/2 -translate-y-1/2"
-						}
+						className="absolute left-[50%] top-[13%] w-[22%] -translate-x-1/2 -translate-y-1/2"
 					/>
 				</motion.div>
 				<Image
 					src={yellow}
 					alt="Yellow Capybara"
-					className={
-						"absolute left-[67%] top-[63%] w-[17%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[67%] top-[63%] w-[17%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={green}
 					alt="Green Capybara"
-					className={
-						"absolute left-[86%] top-[54%] w-[18%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[86%] top-[54%] w-[18%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={basket}
 					alt="basket"
-					className={
-						"absolute left-[80%] top-[68%] w-[13%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[80%] top-[68%] w-[13%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={ant}
 					alt="ant1"
-					className={
-						"absolute left-[90%] top-[80%] w-[15%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[90%] top-[80%] w-[15%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<motion.div
 					className={
@@ -126,30 +108,22 @@ const FooterGraphic = () => {
 				<Image
 					src={ant2}
 					alt="Ant2"
-					className={
-						"absolute left-[11%] top-[47%] w-[12%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[11%] top-[47%] w-[12%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={blue}
 					alt="Blue Capybara"
-					className={
-						"absolute left-[20%] top-[60%] w-[19%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[20%] top-[60%] w-[19%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={red}
 					alt="Red Capybara"
-					className={
-						"absolute left-[32%] top-[72%] w-[24%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[32%] top-[72%] w-[24%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={cherry}
 					alt="Cherry"
-					className={
-						"absolute left-[41%] top-[69%] w-[5%] -translate-x-1/2 -translate-y-1/2"
-					}
+					className="absolute left-[41%] top-[69%] w-[5%] -translate-x-1/2 -translate-y-1/2"
 				/>
 				<Image
 					src={flower}
