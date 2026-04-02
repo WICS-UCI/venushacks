e2196d2ad613d82c5bbe4cf98864f7295a6c5db1
Noah Kim <noahmk5002@gmail.com>
2026-04-02 13:09:34 -0700

    Change apply button path to /choose-role

--- apps/site/src/lib/components/Navbar/Navbar.tsx
+++ apps/site/src/lib/components/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar({ identity }: NavbarProps) {
 						isNavButton
 					/>
 				) : (
-					<Button text="Apply" href="/#apply" usePrefetch={false} isNavButton />
+					<Button text="Apply" href="/choose-role" usePrefetch={false} isNavButton />
 				)}
 			</BaseNavbar>
 
@@ -44,7 +44,7 @@ export default function Navbar({ identity }: NavbarProps) {
 						isNavButton
 					/>
 				) : (
-					<Button text="Apply" href="/#apply" usePrefetch={false} isNavButton />
+					<Button text="Apply" href="/choose-role" usePrefetch={false} isNavButton />
 				)}
 			</div>
 		</div>
