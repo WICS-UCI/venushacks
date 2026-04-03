ae1fd1b4be067e4090e724b1b4a2d627772c15cb
Noah Kim <noahmk5002@gmail.com>
2026-04-03 01:54:46 -0700

    Change IrvineHacks names to VenusHacks

--- apps/site/src/app/(main)/guest-login/page.tsx
+++ apps/site/src/app/(main)/guest-login/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 export const metadata: Metadata = {
-	title: "Guest Login | IrvineHacks 2025",
+	title: "Guest Login | VenusHacks 2026",
 };
 
 export { default as default } from "./GuestLogin";
