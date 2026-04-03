de5f115d2efe396c5381342b6e35c358d9576c21
Kaelyn Sung <sungks@uci.edu>
2026-04-03 09:04:37 -0700

    added animating function

--- apps/site/src/app/(main)/(home)/sections/Landing/PicnicScene.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/PicnicScene.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import React, { useState } from "react";
 
 import OrangeCapybara from "./assets/Orange Capy.svg";
 import YellowCapybara from "./assets/Pineapple Capy.svg";
@@ -9,7 +10,7 @@ import BlueCapybara from "./assets/blueberry capy.svg";
 import BrownCapybara from "./assets/Main Capy.svg";
 import PinkCapybaraLeft from "./assets/cherry_1.svg";
 import PinkCapybaraRight from "./assets/Cherry 2.svg";
-//import Basket from "./assets/picnic_basket.svg";
+import Basket from "./assets/picnic_basket.svg";
 import Blanket from "./assets/thick_picnic_blanket 1.svg";
 import Flower from "./assets/flower.svg";
 import GrassTuft from "./assets/grass.svg";
@@ -20,6 +21,22 @@ import ShadowWide from "./assets/shadow-wide.svg";
 import OpenBasket from "./assets/open_basket.svg";
 
 const PicnicScene = () => {
+	const [isBasketOpen, setIsBasketOpen] = useState(false);
+	const [isAnimating, setIsAnimating] = useState(false);
+
+	const handleBasketHover = () => {
+		if (!isAnimating) {
+			setIsAnimating(true);
+		}
+	};
+
+	const handleBasketAnimationEnd = () => {
+		if (isAnimating) {
+			setIsBasketOpen((prev) => !prev);
+			setIsAnimating(false);
+		}
+	};
+
 	return (
 		<div className="w-full flex justify-center overflow-hidden pb-20 -mt-12">
 			<div className="relative w-full max-w-[1700px] aspect-[17/8]">
@@ -30,11 +47,7 @@ const PicnicScene = () => {
 
 				{/* basket */}
 				<div className="absolute bottom-[19%] left-[47%] -translate-x-1/2 w-[19.5%] z-30">
-					<Image
-						src={OpenBasket}
-						alt="picnic basket"
-						className="w-full h-auto"
-					/>
+					<Image src={Basket} alt="picnic basket" className="w-full h-auto" />
 				</div>
 
 				{/* back row */}
