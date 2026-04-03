b89b347259598feeb0f4deb9e476fd0b1149b8ca
Kaelyn Sung <sungks@uci.edu>
2026-04-02 20:52:03 -0700

    fixed blue gap in small width in home page

--- apps/site/src/app/(main)/(home)/sections/Landing/About/About.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/About/About.tsx
@@ -5,7 +5,7 @@ import flower from "./assets/flower.svg";
 
 const About = () => {
 	return (
-		<section className="relative w-full px-6 pt-16 pb-48 md:px-10 lg:px-16 max-w-[1400px] mx-auto">
+		<section className="relative w-full px-6 pt-16 pb-48 md:px-10 lg:px-16 bg-[#C5C771]">
 			<div className="mx-auto flex items-center justify-between gap-10 w-full">
 				{/* Text Card */}
 				<div className="w-full flex-1 lg:ml-20 rounded-[34px] border-[5px] border-[#d77676] bg-[#e8dfa0] px-10 py-10 md:px-12 md:py-12 lg:px-14 lg:py-12">
