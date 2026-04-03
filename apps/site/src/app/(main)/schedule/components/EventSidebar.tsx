1c606f4ceb2e32af9f1f70abfb729b1d67730408
Noah Kim <noahmk5002@gmail.com>
2026-04-03 13:05:20 -0700

    Remove unused image component

--- apps/site/src/app/(main)/schedule/components/EventSidebar.tsx
+++ apps/site/src/app/(main)/schedule/components/EventSidebar.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef } from "react";
 import clsx from "clsx";
-import Image from "next/image";
 
 import EventPlaque from "./EventPlaque";
 import EventProps from "../EventProps";
