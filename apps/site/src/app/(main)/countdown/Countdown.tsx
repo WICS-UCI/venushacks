cf4d547d7bf26270e10bfbc074ae486d07466c09
Sarah <sarahitan8@gmail.com>
2026-05-13 11:54:16 -0700

    made countdown responsive

--- apps/site/src/app/(main)/countdown/Countdown.tsx
+++ apps/site/src/app/(main)/countdown/Countdown.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import ComingSoonBackground from "../(home)/sections/Landing/ComingSoonBackground";
-import ApplicantPortalBackground from "@/lib/components/ApplicantPortalBackground/ApplicantPortalBackground";
+import CountdownBackground from "./CountdownBackground";
 import logo from "./assets/vh-logo.png";
+import CountdownGrass from "./CountdownGrass";
+import LandingBackgroundGrass from "@/assets/backgrounds/coming-soon-background-grass.svg";
 
 function countdown(deadline: Date) {
 	const deadlineTime = deadline.getTime();
@@ -33,25 +34,33 @@ export default function Countdown() {
 	});
 
 	return (
-		<>
+		<div className="relative w-screen h-screen overflow-hidden">
 			<div className="absolute z-10 right-5 top-5">
-				<Image src={logo} alt="logo" width="100" height="100" />
+				<Image src={logo} className="w-14 md:w-24" alt="logo" width="100" height="100" />
 			</div>
 			<div
-				className="absolute z-10 left-[50%] top-[15%] -translate-x-1/2 -translate-y-1/2
-                        bg-[#D77676] py-2 px-16 rounded-full border-[#B25B5B] border-4"
+				className="absolute z-10 left-[50%] top-[30%] lg:top-[15%] -translate-x-1/2 -translate-y-1/2
+                        bg-[#D77676] py-2 px-13 rounded-full border-[#B25B5B] border-4
+                        w-[80vw] lg:w-[40%] landscape:max-md:w-[60vw] "
 			>
-				<h2 className="font-torus text-[40px] text-white text-center tracking-[10%]">
+				<h2 className="font-torus text-[20px] md:text-[40px] text-white text-center tracking-[10%]">
 					Hacking ends in...
 				</h2>
 			</div>
-			<div className="absolute z-10 left-[50%] top-[37%] -translate-x-1/2 -translate-y-1/2">
-				<h2 className="font-sniglet font-bold text-[200px] text-[#2F3248]">
+			<div className="absolute z-10 left-[50%] top-[50%] lg:top-[37%] -translate-x-1/2 -translate-y-1/2">
+				<h2 className="font-sniglet font-bold text-[90px] md:text-[200px] text-[#2F3248]">
 					{time}
 				</h2>
 			</div>
-			<ComingSoonBackground />
-			<ApplicantPortalBackground />
-		</>
+            <Image
+				src={LandingBackgroundGrass}
+				alt="Landing Background Grass"
+				className="absolute z-10 bottom-8 md:-bottom-10 left-1/2 -translate-x-1/2 w-full 
+                        scale-150 md:scale-100 landscape:max-md:scale-100 landscape:max-md:-bottom-10"
+			/>
+            <div className="absolute -z-10">
+			    <CountdownBackground/>
+            </div>
+		</div>
 	);
 }
---
+++ apps/site/src/app/(main)/countdown/CountdownBackground.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import ApplicantPortalGrass from "@/assets/applicant-portal/applicant-portal-grass.png";
+import CloudGroup from "@/assets/backgrounds/coming-soon-clouds.svg";
+import styles from "@/lib/components/ApplicantPortalBackground/ApplicantPortalBackground.module.css";
+
+export default function CountdownBackground() {
+	return (
+		<div className="fixed inset-0 -z-10 bg-[#B2EEE7]">
+			{/* Clouds */}
+			<div className={`${styles.cloudTrack} absolute top-[5%] md:top-0 left-0`}>
+				<Image
+					src={CloudGroup}
+					alt="Clouds"
+					className="w-[180vw] md:w-screen h-auto flex-shrink-0"
+				/>
+				<Image
+					src={CloudGroup}
+					alt="Clouds"
+					className="w-[180vw] md:w-screen h-auto flex-shrink-0"
+				/>
+			</div>
+		</div>
+	);
+}
