b1d73fda01555391ceaae6e53a36cfbffda6e78a
Sarah <sarahitan8@gmail.com>
2026-05-13 09:45:03 -0700

    added countdown path + graphics

---
+++ apps/site/src/app/(main)/countdown/Countdown.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useState, useEffect } from "react";
+import ComingSoonBackground from "../(home)/sections/Landing/ComingSoonBackground";
+import ApplicantPortalBackground from "@/lib/components/ApplicantPortalBackground/ApplicantPortalBackground";
+
+function countdown(deadline: Date) {
+    const deadlineTime = deadline.getTime();
+    const now = new Date().getTime();
+    const difference = deadlineTime - now;
+
+    const hours = Math.floor((difference) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+
+export default function Countdown(){
+    const DEADLINE = new Date("May 17, 2026 13:00:00");
+    const [time, setTime] = useState<string | null>(null);
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(countdown(DEADLINE));
+    }, 1000);
+
+    return () => clearInterval(interval); // cleanup on unmount
+  });
+
+  return(
+    <>
+        <div className="absolute z-10 left-[50%] top-[15%] -translate-x-1/2 -translate-y-1/2
+                        bg-[#D77676] py-2 px-24 rounded-full border-[#B25B5B] border-4">
+            <h2 className="font-torus text-[48px] text-white text-center tracking-[10%]">Hacking ends in...</h2>
+        </div>
+        <div className="absolute z-10 left-[50%] top-[35%] -translate-x-1/2 -translate-y-1/2">
+            <h2 className="font-torus font-bold text-[220px] text-[#2F3248]">
+                {time}
+            </h2>
+        </div>
+        <ComingSoonBackground/>
+        <ApplicantPortalBackground />
+    </>
+  );
+}
