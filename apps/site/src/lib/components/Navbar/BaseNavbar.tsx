e5f0f45212d211a6edc230382abee874252ef383
Sarah <sarahitan8@gmail.com>
2026-05-14 11:54:59 -0700

    changed navbar order

--- apps/site/src/lib/components/Navbar/BaseNavbar.tsx
+++ apps/site/src/lib/components/Navbar/BaseNavbar.tsx
@@ -17,10 +17,9 @@ export default function BaseNavbar({ children }: PropsWithChildren) {
 						<NavLinkItem href="/#home">Home</NavLinkItem>
 						<NavLinkItem href="/#about">About</NavLinkItem>
 						<NavLinkItem href="/#faqs">FAQs</NavLinkItem>
-						{/* <NavLinkItem href="/#partners">Partners</NavLinkItem> */}
-						<NavLinkItem href="/#prizes">Prizes</NavLinkItem>
 						<NavLinkItem href="/#sponsors">Sponsors</NavLinkItem>
 						<NavLinkItem href="/#partners">Partners</NavLinkItem>
+						<NavLinkItem href="/#prizes">Prizes</NavLinkItem>
 						<NavLinkItem href="/#team">Team</NavLinkItem>
 					</NavMenu.List>
 					<NavMenu.Indicator className={styles.navMenuIndicator} />
