dd7aa573de7aacf614a8b8152359b92479f8ad6d
Sarah <sarahitan8@gmail.com>
2026-05-15 21:32:13 -0700

    prettier

--- apps/site/src/app/(main)/schedule/page.tsx
+++ apps/site/src/app/(main)/schedule/page.tsx
@@ -18,8 +18,21 @@ export default async function Schedules() {
 			</div>
 			<section className="container py-20 md:py-24 relative mx-auto max-w-screen-2xl bg-[#BEE7E1] px-6 md:px-14">
 				<div className="flex justify-center items-center flex-col gap-10">
-					<Image priority src={sched1} width={800} height={500} alt="day 1 schedule" className="border-[5px] md:border-[20px] border-white rounded-[30px] md:rounded-[50px]" />
-					<Image src={sched2} width={800} height={500} alt="day 2 schedule" className="border-[5px] md:border-[20px] border-white rounded-[30px] md:rounded-[50px]" />
+					<Image
+						priority
+						src={sched1}
+						width={800}
+						height={500}
+						alt="day 1 schedule"
+						className="border-[5px] md:border-[20px] border-white rounded-[30px] md:rounded-[50px]"
+					/>
+					<Image
+						src={sched2}
+						width={800}
+						height={500}
+						alt="day 2 schedule"
+						className="border-[5px] md:border-[20px] border-white rounded-[30px] md:rounded-[50px]"
+					/>
 				</div>
 			</section>
 		</>
